dd3d9f7af3e0abd8fafdaf49ac7640422d2d3640
Joaquin Gavernet <joaquingavernet@gmail.com>
2023-07-14 14:26:23 -0300

    Ejercicio F finalizado y aceptado.

---
+++ binary-search/step2/F.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define RAYA cerr << "===============================" << endl
+#define SZ(x) int(x.size())
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+bool good(int n, int sizeP, string &t, string &p) {
+	int i = 0, j = 0;
+	forn(i,n) if (t[i] == p[j]) j++;
+	if (j == sizeP) return true;
+	return false;
+}
+
+int main() {
+	FIN;
+	
+	string t;
+	string p;
+
+	cin >> t >> p;
+	int n = SZ(t);
+	int sizeP = SZ(p);
+	vector<int> deletions(n);
+	forn(i,n) cin >> deletions[i],deletions[i]--;
+	
+	int l = 0;
+	int r = n;
+	
+	while (r > l + 1) {
+		string copia = t;
+		int m = (l + r) / 2;
+		forn(i,m) copia[deletions[i]] = '0';
+		bool answer = good(n, sizeP, copia, p);
+		if (answer) l = m;
+		else r = m;
+	}
+
+	cout << l << "\n";
+	
+	return 0;
+}
