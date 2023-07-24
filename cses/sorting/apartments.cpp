79fc52d3d096db79950f713f25ceed2629a7b056
Joaquin Gavernet <joaquingavernet@gmail.com>
2023-07-24 00:58:18 -0300

    Problem sorting/apartments accepted.

---
+++ cses/sorting/apartments.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define RAYA cerr << "===============================" << endl
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+
+int main(){
+  FIN;
+  
+  ll n, m, k; cin >> n >> m >> k;
+  vector<ll> applicants(n), apartments(m);
+  forn(i,n) cin >> applicants[i];
+  forn(i,m) cin >> apartments[i];
+  
+  sort(applicants.rbegin(),applicants.rend());
+  sort(apartments.rbegin(),apartments.rend());
+
+  dbg(applicants);
+  dbg(apartments);
+  
+  ll tot = 0;
+  
+  ll x = 0, y = 0;
+  
+  while (x < n and y < m) {
+    ll app = applicants[x];
+    ll places = apartments[y];
+    if (max(app,places) - min(app,places) <= k) {
+      tot++;
+      x++;
+      y++;
+    } else if (places < app) x++;
+    else y++;
+  }
+  
+  cout << tot << "\n";
+  
+  return 0;
+}
+
+
