9d3547620039d54d4eb2dd4a63611b094322503b
Joaquin Gavernet <joaquingavernet@gmail.com>
2023-10-18 00:41:48 -0300

    Added solution to first introductory problem.

---
+++ cses/introductory-problems/missing-number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define forr(i,a,b) for (ll i = a; i < (ll)(b); i++)
+#define forn(i,n) forr(i,0,n)
+
+int main() {
+  
+  int n,a; cin >> n;
+  set<int> s;
+  
+  while (cin >> a) s.push(a);
+  
+  forn(i,n)
+  
+
+  return 0;
+}
