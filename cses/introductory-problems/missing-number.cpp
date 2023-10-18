d42b0711ad50cb71cc3f1086ebc6e2f82c81f42e
Joaquin Gavernet <joaquingavernet@gmail.com>
2023-10-18 01:00:32 -0300

    Solved missing-number in introductory problems.

--- cses/introductory-problems/missing-number.cpp
+++ cses/introductory-problems/missing-number.cpp
@@ -8,12 +8,10 @@ typedef long long ll;
 int main() {
   
   int n,a; cin >> n;
-  set<int> s;
+  map<int,bool> s;
   
-  while (cin >> a) s.push(a);
+  while (cin >> a) s.insert({a,1});  
+  for (int i = 1; i <= n; i++) if (s[i] == 0) cout << i;
   
-  forn(i,n)
-  
-
   return 0;
 }
