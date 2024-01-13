31aec502edd788ee4dc68f12b74a11cf4e3fb7e4
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-01-13 16:11:27 -0300

    Solved with binary-search.

---
+++ cses/sorting/factory-machines.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define RAYA cerr << "===============================" << endl
+const ll MOD = (ll)(1e12+7); // 998244353 
+const ll INF = (ll)(1LL<<60); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+
+int main(){
+  FIN;
+  
+  ll n,p; cin >> n >> p;
+  vector<ll> v(n); forn(i,n) cin >> v[i];
+  
+  ll l = 0;
+  ll r = INF;
+  while (l < r-1) { // O(nlogn)
+    ll mid = l + (r-l)/2;
+    ll tot = 0;
+    forn(i,n) tot += floor(mid/v[i]), tot %= MOD; // O(n)
+    if (tot < p) l = mid;
+    else r = mid;
+  }
+    
+  cout << r << endl;
+  
+  return 0;
+}
+
+
+
+
+
+
