c230b700396aa634e4c0acc17f9196479939378b
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-01-20 21:10:21 -0300

    TLE in tests 2 and 3.

---
+++ cses/additional-problems/movie-festival-queries.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define RAYA cerr << "===============================" << endl
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const ll LOG = 20;
+
+//~ const int MAXN = (int)(200);
+const int MAXN = (int)(2e6+5);
+#define pi pair<ll,ll>
+
+bool f(pi a, pi b) {
+  return (a.second == b.second) ? a.first < b.first : a.second < b.second;
+}
+
+int main(){
+  FIN;
+  
+  vector<vector<ll>> t(MAXN, vector<ll>(LOG,INF)); // sparse table
+  int n,q; cin >> n >> q;
+  vector<pi> v(n); 
+  ll top = -MAXN;
+  forn(i,n) {
+    cin >> v[i].first >> v[i].second;
+    t[v[i].first][0] = min(t[v[i].first][0],v[i].second);
+    top = max(top,v[i].second);
+  }
+  sort(all(v),f);
+  
+  // preprocessing sparse table
+  for (ll i = top-2; i >= 0; i--) {
+    t[i][0] = min(t[i+1][0], t[i][0]);
+  }
+  
+  // solving queries online in O(log2(n)) 
+  while (q--) {
+    ll l,r; cin >> l >> r;
+    ll pos = l, cont = 0;
+    
+    while (pos <= r) {
+      if (t[pos][0] <= r) cont++, pos = t[pos][0];
+      else break;
+    }
+    cout << cont << '\n';
+  }
+  
+  
+  
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
