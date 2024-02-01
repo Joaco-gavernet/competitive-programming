5d0e3ab55cd2ea0451a31e3c11709f98bee10e70
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-02-01 15:21:45 -0300

    Solved with floyd's algorithm for cycle detection after 8+ hours of debugging.

--- cses/graph-algorithms/planets-cycles.cpp
+++ cses/graph-algorithms/planets-cycles.cpp
@@ -40,65 +40,74 @@ const int MAXN = (int)(2e5+5);
 // else if v is not in a cycle: 
 // - ans[v].f == 0
 // - ans[v].s == distance to proximum cycle
-vector<pair<int,int>> findCycles(int x, vector<int> &succ) {
+vector<pair<int,int>> floydAlgorithm(int x, vector<int> &succ) {
   int n = succ.size();
   vector<pair<int,int>> ans(n);
   vector<bool> visto(n);
   int tot = 0;
-  dbg(succ);
 
   while (tot < n) {
     // check if there is any cycle
-    int a = succ[x];
+    stack<int> s; s.push(x);
+    int a = succ[x]; 
     int b = succ[succ[x]];
-    dbg(x);
-    dbg(a,b);
 
-    while (a != b) {
-      if (a == succ[a] and b == succ[succ[b]]) break;
+    while (a != b and visto[a] == 0) {
+      s.push(a);
       a = succ[a];
       b = succ[succ[b]];
     }
+
+    if (visto[a] == 1) { // use already computed nodes for efficiency
+      while (s.size()) { // update visited nodes
+        a = s.top(); s.pop();
+        visto[a] = 1; tot++;
+        ans[a].f = 0; // path to cycle
+        ans[a].s = ans[succ[a]].s +1; // consider distance to start of cycle
+      }
+    } else {
+      // find first node that belongs to the cycle
+      int dist = 0;
+      a = x;
+      while (a != b) {
+        a = succ[a];
+        dist++;
+        b = succ[b];
+      }
+      int start = a;
+
+      // calculate the length of the cycle
+      b = succ[a];
+      int length = 1;
+      while (start != b) {
+        length++;
+        b = succ[b];
+      }
+
+      // update ans for cycle found
+      ans[start] = {1,length};
+      if (visto[start] == 0) visto[start] = 1, tot++;
+      b = succ[start];
+      while (b != start) {
+        if (visto[b] == 1) break;
+        ans[b] = {1,length};
+        visto[b] = 1, tot++;
+        b = succ[b];
+      }
+      dist += ans[start].s;
+
+      // update ans for path from start to cycle
+      if (x != start) { 
+        b = x;
+        while (b != start) {
+          if (visto[b] == 1) break;
+          ans[b] = {0,dist--};
+          visto[b] = 1, tot++;
+          b = succ[b];
+        }
+      }
+    }
     
-    // find first node that belongs to the cycle
-    a = x;
-    while (a != b) {
-      a = succ[a];
-      b = succ[b];
-    }
-    int start = a;
-
-    // calculate the length of the cycle
-    b = succ[a];
-    int length = 1;
-    while (start != b) {
-      length++;
-      b = succ[b];
-    }
-
-    // update ans for cycle found
-    ans[start] = {1,length};
-    visto[start] = 1;
-    tot++;
-    b = succ[start];
-    while (b != start) {
-      ans[b] = {1,length};
-      tot++;
-      visto[b] = 1;
-      b = succ[b];
-    }
-
-    int dist = 1;
-    while (x != start) {
-      if (visto[x] == 1) break;
-      ans[x] = {0,dist};
-      dist; tot++;
-      visto[x] = 1;
-      x = succ[x];
-    }
-    
-    x = 0;
-    dbg(visto);
     while (tot < n and visto[x] == 1) x++;
   }
 
@@ -115,23 +124,10 @@ int main(){
     succ[i]--;
   }
 
-  vector<pair<int,int>> cycles = findCycles(0,succ);
-  dbg(cycles);
-
-  vector<int> ans(n);
-  forn(i,n) {
-    if (cycles[i].f == 1) ans[i] = cycles[i].s;
-    else {
-      int result = cycles[i].s; // distance to proximum cycle
-      int act = i;
-      while (cycles[act].f == 0) act = succ[act]; 
-      result += cycles[act].s;
-      ans[i] = result;
-    }
-  }
+  vector<pair<int,int>> cycles = floydAlgorithm(0,succ);
 
   // print result
-  for(int i: ans) cout << i << ' ';
+  for(auto [a,b] : cycles) cout << b << ' ';
   cout << endl;
   
   
