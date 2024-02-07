b35998ae043eaf82639f2f016a88ae9a07dbad88
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-02-07 16:08:08 -0300

    Upsolved with minmax-segtree.

--- rounds/round-923/D.cpp
+++ rounds/round-923/D.cpp
@@ -31,6 +31,52 @@ const ll MOD = (ll)(1e9+7); // 998244353
 const ll INF = (ll)(1<<30); // (1LL<<60)
 const int MAXN = (int)(2e5+5);
 
+#define f first
+#define s second
+
+struct tup{
+  int mx,mi;
+  int imx, imi;
+};
+
+typedef tup tipo;
+// first = max
+// second = min
+struct segtree {
+  vector<tipo> v;
+  vector <tipo> t; int tam;
+  int NEUTmax = -1, NEUTmin = INF; // Neutral element of operation
+  tipo op(tipo a, tipo b) {
+    tipo res = {NEUTmax, NEUTmin, -1, -1};
+    if (a.mx >= b.mx) res.mx = a.mx, res.imx = a.imx; 
+    else res.mx = b.mx, res.imx = b.imx;
+
+    if (a.mi <= b.mi) res.mi = a.mi, res.imi = a.imi;
+    else res.mi = b.mi, res.imi = b.imi;
+
+    return res;
+  }
+   
+  void build(vector<tipo> _v, int n) {  // build the tree 
+    v = _v;
+    // root en 1, ojas en el intervalo [tam, 2*tam-1]
+    tam = sizeof(int) * 8 - __builtin_clz(n); tam = 1<<tam;
+    t.resize(2*tam,{NEUTmax, NEUTmin, -1, -1}); 
+
+    forn(i, n) t[tam+i] = v[i];
+    for(int i = tam - 1; i > 0; i--) t[i] = op(t[i<<1], t[i<<1|1]);
+  }
+
+  tipo query(int l, int r) {  // op on interval [l, r]
+    tipo res = {NEUTmax, NEUTmin, -1, -1};
+    for (l += tam, r += tam; l <= r; l >>= 1, r >>= 1) {
+      if (l&1) res = op(res, t[l++]);
+      if (!(r&1)) res = op(res, t[r--]);
+    }
+    return res;
+  }
+};
+
 
 int main(){
   FIN;
@@ -38,24 +84,25 @@ int main(){
   int t; cin >> t;
   while (t--) {
     int n; cin >> n;
-    vector<int> v(n); forn(i,n) cin >> v[i];
+    vector<tipo> a(n); forn(i,n) cin >> a[i].mx, a[i].mi = a[i].mx, a[i].imi = a[i].imx = i;
+
+    segtree s;
+    s.build(a,n);
 
     int q; cin >> q;
     while (q--) {
       int l,r; cin >> l >> r;
       l--; r--;
 
-      while (l != r) {
-        if (v[l] != v[r]) break;
-        r--;
-      }
-      if (l == r) cout << "-1 -1\n";
-      else cout << l +1 << ' ' << r +1 << '\n';
+      tipo ans = s.query(l,r);
+      if (ans.imx == ans.imi) cout << "-1 -1";
+      else cout << ans.imx +1 << ' ' << ans.imi +1;
+      cout << '\n';
     }
     cout << '\n';
   }
-  
-  
-  
+
+
+
   return 0;
 }
