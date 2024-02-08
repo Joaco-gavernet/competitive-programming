f0a1fbe82c758ef7556d07a0f33eb15b0c1a80c2
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-02-07 22:14:46 -0300

    Solved with lazy-propagation-segtree.

--- cses/range-queries/range-update-queries.cpp
+++ cses/range-queries/range-update-queries.cpp
@@ -31,57 +31,85 @@ const ll MOD = (ll)(1e9+7); // 998244353
 const ll INF = (ll)(1<<30); // (1LL<<60)
 const int MAXN = (int)(2e5+5);
 
+typedef long long tipo;
+const int NEUT = 0; // REMINDER !!! 
 
-typedef ll tipo;
-struct segtree {
-  vector<tipo> t; int tam;
-  tipo NEUT = 0;
-  tipo op(tipo a, tipo b){ return a + b; }
+struct node {
+  tipo ans, l, r, lazy = 0;
+  bool upd = false;
+  node() {ans = lazy = 0; upd = false; l = r = -1;} // REMINDER !!! SET NEUT
+  node(tipo val, int pos) : ans(val), l(pos), r(pos) {} // Set node
+  void set_lazy(tipo x) {lazy += x; upd = true;}
+};
 
-  void build(vector<tipo> v, int n) { // build the tree
-    // root en 1, ojas en el intervalo [tam, 2*tam-1]
-    tam = sizeof(int) * 8 - __builtin_clz(n);
-    tam = 1 << tam; //Primera potencia de 2 mayor a n
-    t.resize(2 * tam, 0); forn(i, n) t[tam+i] = v[i];
-    for(int i = tam - 1; i > 0; i--)
-      t[i] = op(t[i << 1], t[i << 1 | 1]); //Poner la operacion del seg
+struct segtree_lazy {
+  #define l(x) int(x<<1)
+  #define r(x) int(x<<1|1)
+
+  vector <node> t; int tam;
+
+  node op(node a, node b) {
+    node aux; aux.ans = a.ans + b.ans; //Operacion de query
+    aux.l = a.l; aux.r = b.r;
+    return aux;
   }
 
-  void modify(int l, int r, tipo value) {
-    for (l += tam, r += tam; l <= r; l >>= 1, r >>= 1) {
-      dbg(l,r);
-      if (l&1) t[l] = op(t[l], value); // Poner la operacion del seg
-      if (!(r&1)) t[r] = op(t[r], value); // Poner la operacion del seg
-      l++, r--;
+  void push(int p) {
+    node &cur = t[p];
+    if(cur.upd == true) {
+      cur.ans += cur.lazy * (cur.r-cur.l+1); //Operacion update
+      if(cur.l < cur.r) {
+        t[l(p)].lazy += cur.lazy; t[l(p)].upd = true;
+        t[r(p)].lazy += cur.lazy; t[r(p)].upd = true;
+      }
+      cur.lazy = 0; cur.upd = false; //Poner el neutro del update
     }
   }
 
-  tipo query(int p) {
-    dbg(t);
-    tipo res = NEUT;
-    for (p += tam; p > 0; p >>= 1) res = op(res, t[p]); //Operacion del seg
-    return res;
+  node query(int l, int r, int p = 1) {
+    push(p); node &cur = t[p];
+    if(l > cur.r || r < cur.l) return node(); // Return NEUT
+    if(l <= cur.l && cur.r <= r) return cur;
+    return op(query(l,r,l(p)),query(l,r,r(p)));
+  }
+
+  void update(int l, int r, tipo val, int p = 1) { // root at p = 1
+    push(p); node &cur = t[p];
+    if(l > cur.r || r < cur.l) return;
+    if(l <= cur.l && cur.r <= r) {
+      cur.set_lazy(val); push(p); return;
+    }
+    update(l, r, val, l(p)); update(l, r, val, r(p));
+    cur = op(t[l(p)], t[r(p)]);
+
+  }
+
+  void build(vector <tipo> v, int n) { // iterative build
+    tam = sizeof(int) * 8 - __builtin_clz(n); tam = 1<<tam;
+    t.resize(2*tam); v.resize(tam);
+    forn(i,tam) t[tam+i] = node(v[i],i);
+    for(int i = tam - 1; i > 0; i--) t[i] = op(t[l(i)],t[r(i)]); 
   }
 };
 
-
-int main(){
+int main() {
   FIN;
 
   int n, q; cin >> n >> q;
   vector<tipo> v(n); forn(i,n) cin >> v[i];
 
-  segtree s;
+  segtree_lazy s;
   s.build(v,n);
 
   while (q--) {
     int t; cin >> t;
     if (t==1) {
       int a,b,u; cin >> a >> b >> u;
-      s.modify(--a,--b,u);
+      s.update(--a,--b,u);
     } else {
       int k; cin >> k;
-      cout << s.query(--k) << '\n';
+      k--;
+      cout << s.query(k,k).ans << '\n';
     }
   }
 
