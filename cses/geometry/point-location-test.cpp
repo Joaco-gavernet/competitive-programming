68e7cf00faf0ffa9b22a18fe17bdde81274e514d
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-02-10 23:36:09 -0300

    Solved with cross product, vector prolongation interpretation.

---
+++ cses/geometry/point-location-test.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define RAYA cerr << "===============================" << endl
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+
+typedef ll C;
+typedef complex<C> P;
+#define X real()
+#define Y imag()
+
+int main(){
+  FIN;
+
+  int t; cin >> t;
+
+  ll x,y;
+  while (t--) {
+    cin >> x >> y;
+    P a = {x,y};
+    cin >> x >> y;
+    P b = {x,y};
+    cin >> x >> y;
+    P c = {x,y};
+
+    P s1 = c-a;
+    P s2 = c-b;
+
+    s1 = {s1.X, -s1.Y};
+
+    ll res = (s1*s2).Y; // cross product (c-a) x (c-b) 
+    if (res > 0) cout << "LEFT\n";
+    else if (res < 0) cout << "RIGHT\n";
+    else cout << "TOUCH\n";
+  }
+
+  
+  return 0;
+}
