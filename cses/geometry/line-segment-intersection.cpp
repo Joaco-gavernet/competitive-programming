82e163c42b17e3000b43412bf3a1798eb9deac9b
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-02-15 01:58:46 -0300

    Solved with struct punto, struct recta, and inter_seg() from notebook.

--- cses/geometry/line-segment-intersection.cpp
+++ cses/geometry/line-segment-intersection.cpp
@@ -6,10 +6,10 @@ using namespace std;
 // neal Debugger
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
+ 
 void dbg_out() { cerr << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
-
+ 
 #ifdef LOCAL
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
 #else
@@ -31,78 +31,92 @@ const ll MOD = (ll)(1e9+7); // 998244353
 const ll INF = (ll)(1<<30); // (1LL<<60)
 const int MAXN = (int)(2e5+5);
 
+typedef long double tipo; //Cambiar a long long para operar en enteros
+double EPS = (double)(1e-10);
 
-typedef ll C;
-typedef complex<C> P;
-#define X real()
-#define Y imag()
-#define r(x) (x).real()
-
-bool sameLine(P a, P b, P c, P d) {
-  if ((conj(a)*c).Y == 0 and (conj(a)*d).Y == 0) return 1;
-  else return 0;
-}
-
-bool correctOrder(P a, P b, P c, P d) {
-  if (min(r(a),r(b))<=min(r(c),r(d)) and max(r(c),r(d))<=max(r(a),r(b))) return 1;
-  else if (min(r(c),r(d))<= min(r(a),r(b)) and max(r(a),r(b))<=max(r(c),r(d))) return 1;
-  else if (min(r(a),r(b))<=r(c) and r(c)<=max(r(a),r(b))) return 1;
-  else if (min(r(a),r(b))<=r(d) and r(d)<=max(r(a),r(b))) return 1;
-  else return 0;
-}
-
-ll side(P x, P y, P z) {
-  return (conj(z-x)*(z-y)).Y; // cross product (z-x) x (z-y) 
-}
-
-bool intersect(P a, P b, P c, P d) {
-  ll res1 = side(a,b,c);
-  if (res1 == 0) return 1;
-  ll res2 = side(a,b,d);
-  if (res2 == 0) return 1;
-  ll res3 = side(c,d,a);
-  if (res3 == 0) return 1;
-  ll res4 = side(c,d,b);
-  if (res4 == 0) return 1;
-
-  if ((res1<=0 and res2>=0) or (res1>=0 and res2<=0)){
-    if ((res3<=0 and res4>=0) or (res3>=0 and res4<=0)) return 1;
-    else return 0;
+struct punto { // Puede usarse para vectores
+  tipo x, y;
+  punto const operator -(const punto &p) const {return {x-p.x,y-p.y};}
+  punto const operator +(const punto &p) const {return {x+p.x,y+p.y};}
+  tipo operator *(const punto &p) const {return x*p.x + y*p.y;}
+  tipo operator ^(const punto &p) const {return x*p.y - y*p.x;}
+  bool operator == (const punto &p) const {
+    return (abs(x-p.x) < EPS && abs(y-p.y) < EPS); // Para double
   }
-  else return 0;
+  bool operator <(punto p) const {return x != p.x ? x < p.x : y < p.y;}
+  tipo arg() {return atan2(y,x);} 
+  tipo mod() {return sqrtl(x*x + y*y);}
+  tipo mod2() {return x*x + y*y;}
+};
+
+punto operator*(tipo k, const punto &p) {return {k*p.x, k*p.y};}
+
+ostream &operator << (ostream &os, const punto &p) { //Para imprimir
+  return os << "(" << p.x << "," << p.y << ")";
 }
 
+istream &operator >> (istream &is, punto &p) { //Para leer
+  return is >> p.x >> p.y;
+}
+
+struct recta { // Puede usarse para segmentos ([p,p+v] o alpha = [0,1])
+  punto v, p; // v -> director, p -> punto por donde pasa
+  recta(punto p1, punto p2) { v = (p2-p1); p = p1;}
+  recta() {}
+  recta(tipo A, tipo B, tipo C) { // Transform Ax + By + C = 0 
+    v = {-B,A}; A != 0 ? p = {-C / A,0} : p = {0,-C / B}; 
+  }
+  bool is_in(punto q){return fabs((q.x-p.x)*v.y - (q.y-p.y)*v.x) < EPS;}
+  punto eval(double x) {return x * v + p;}
+};
+
+bool inter_seg(recta &r1, recta &r2, punto &ans) {
+  if(r1.p == r2.p || r1.p == r2.p+r2.v) {ans = r1.p; return true;}
+  if(r1.p+r1.v == r2.p || r1.p+r1.v == r2.p+r2.v) { 
+    ans = r1.p+r1.v; return true; } //Casos que coincidan extremos
+  if(abs(r1.v ^ r2.v) < EPS) return false; // son paralelos
+  tipo alpha = tipo((r2.p - r1.p)^r2.v) / tipo(r1.v^r2.v);
+  tipo beta = tipo((r1.p - r2.p)^r1.v) / tipo(r2.v^r1.v);
+  if(alpha < -EPS || beta < -EPS) return false;
+  if(alpha > 1.0+EPS || beta > 1.0+EPS) return false;
+  ans = r1.p + alpha * r1.v; return true;
+} 
+
+
+bool sameLine(recta x, recta y) {
+  return (abs(x.v^y.v) < EPS and abs((y.p-x.p)^(y.p-(x.p+x.v))) < EPS);
+}
+
+bool correctOrder(recta x, recta y) {
+  tipo a,b,c,d;
+  if (x.v.x == 0 and y.v.x == 0) {
+    a = x.p.y, b = (x.p+x.v).y;
+    c = y.p.y, d = (y.p+y.v).y;
+  } else {
+    a = x.p.x, b = (x.p+x.v).x;
+    c = y.p.x, d = (y.p+y.v).x;
+  }
+  if (min(a,b)<=min(c,d) and max(c,d)<=max(a,b)) return 1;
+  else if (min(c,d)<=min(a,b) and max(a,b)<=max(c,d)) return 1;
+  else if (min(a,b)<=c and c<=max(a,b)) return 1;
+  else if (min(a,b)<=d and d<=max(a,b)) return 1;
+  else return 0;
+}
 
 int main(){
   FIN;
 
-  // There are three possible cases for valid intersections: 
-  // 1. They intersect exactly in one vertex. 
-  // 2. Segments are in the same line and they overlap each other.
-  // 3. They intersect in the middle. 
-
   int t; cin >> t;
+  punto a,b,c,d,z; 
 
-  ll x,y;
   while (t--) {
-    cin >> x >> y;
-    P a = {x,y};
-    cin >> x >> y;
-    P b = {x,y};
-    cin >> x >> y;
-    P c = {x,y};
-    cin >> x >> y;
-    P d = {x,y};
+    cin >> a >> b >> c >> d;
 
-    dbg(sameLine(a,b,c,d));
-    dbg(correctOrder(a,b,c,d));
-
-    if (a == c or a == d or b == c or b == d) cout << "YES\n"; 
-    else if (sameLine(a,b,c,d) and correctOrder(a,b,c,d)) cout << "YES\n";
-    else if (intersect(a,b,c,d)) cout << "YES\n";
+    recta x(a,b), y(c,d);
+    if (inter_seg(x,y,z)) cout << "YES\n";
+    else if (sameLine(x,y) and correctOrder(x,y)) cout << "YES\n";
     else cout << "NO\n";
   }
-
 
   return 0;
 }
