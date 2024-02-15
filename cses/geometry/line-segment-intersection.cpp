e79144c4eb93230694dfd99d646d481cd7aa2db3
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-02-14 23:01:23 -0300

    Failed attempt with complex representation.

--- cses/geometry/line-segment-intersection.cpp
+++ cses/geometry/line-segment-intersection.cpp
@@ -36,6 +36,7 @@ typedef ll C;
 typedef complex<C> P;
 #define X real()
 #define Y imag()
+#define r(x) (x).real()
 
 bool sameLine(P a, P b, P c, P d) {
   if ((conj(a)*c).Y == 0 and (conj(a)*d).Y == 0) return 1;
@@ -43,25 +44,26 @@ bool sameLine(P a, P b, P c, P d) {
 }
 
 bool correctOrder(P a, P b, P c, P d) {
-  if (a.real() <= c.real() and d.real() <= b.real()) return 1;
-  else if (c.real() <= a.real() and b.real() <= d.real()) return 1;
-  else if (a.real() <= c.real() and c.real() <= b.real()) return 1;
-  else if (a.real() <= d.real() and d.real() <= b.real()) return 1;
-  // else if (c.real() <= b.real() and b.real() <= d.real()) return 1;
+  if (min(r(a),r(b))<=min(r(c),r(d)) and max(r(c),r(d))<=max(r(a),r(b))) return 1;
+  else if (min(r(c),r(d))<= min(r(a),r(b)) and max(r(a),r(b))<=max(r(c),r(d))) return 1;
+  else if (min(r(a),r(b))<=r(c) and r(c)<=max(r(a),r(b))) return 1;
+  else if (min(r(a),r(b))<=r(d) and r(d)<=max(r(a),r(b))) return 1;
   else return 0;
 }
 
 ll side(P x, P y, P z) {
-  P s1 = z-x;
-  P s2 = z-y;
-  return (conj(s1)*s2).Y; // cross product (z-x) x (z-y) 
+  return (conj(z-x)*(z-y)).Y; // cross product (z-x) x (z-y) 
 }
 
 bool intersect(P a, P b, P c, P d) {
   ll res1 = side(a,b,c);
+  if (res1 == 0) return 1;
   ll res2 = side(a,b,d);
+  if (res2 == 0) return 1;
   ll res3 = side(c,d,a);
+  if (res3 == 0) return 1;
   ll res4 = side(c,d,b);
+  if (res4 == 0) return 1;
 
   if ((res1<=0 and res2>=0) or (res1>=0 and res2<=0)){
     if ((res3<=0 and res4>=0) or (res3>=0 and res4<=0)) return 1;
@@ -74,8 +76,7 @@ bool intersect(P a, P b, P c, P d) {
 int main(){
   FIN;
 
-  // Observations:
-  // There are three possible cases
+  // There are three possible cases for valid intersections: 
   // 1. They intersect exactly in one vertex. 
   // 2. Segments are in the same line and they overlap each other.
   // 3. They intersect in the middle. 
