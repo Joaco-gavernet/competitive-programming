1ab270596b42b6e6bde2803f6d6fdb8434efaeae
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-02-17 20:22:11 -0300

    Solved with matrix-exponentiation with operator* modified to min() and different identity matrix.

---
+++ cses/mathematics/graph-paths-ii.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define RAYA cerr << "===============================" << endl
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1LL<<60); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+typedef vector<vector<ll>> Matrix;
+
+Matrix ones(int n) {
+  Matrix r(n,vector<ll>(n,INF));
+  r[0][0] = 0;
+  return r;
+}
+
+Matrix operator*(Matrix &a, Matrix &b) {
+  int n=a.size(),m=b[0].size(),z=a[0].size();
+  Matrix r(n,vector<ll>(m,INF));
+  forn(i,n)forn(j,m)forn(k,z)
+    r[i][j] = min(r[i][j],a[i][k]+b[k][j]);
+  return r;
+}
+
+Matrix be(Matrix b, ll e) {
+  Matrix r=ones(b.size());
+  while(e){if(e&1LL)r=r*b;b=b*b;e/=2;}
+  return r;
+}
+
+int main(){
+  FIN;
+
+  ll n,m,k; cin >> n >> m >> k;
+  Matrix g(n, vector<ll>(n,INF));
+
+  ll a,b,c;
+  while (m--) {
+    cin >> a >> b >> c;
+    g[a][b] = min(c,g[--a][--b]);
+  }
+
+  Matrix ans = be(g,k);
+  cout << ((ans[0][n-1] == INF) ? -1 : ans[0][n-1]) << '\n';
+
+
+  return 0;
+}
