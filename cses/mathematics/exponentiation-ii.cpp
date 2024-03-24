151770f28dcbddcb8e26b90501ef4c2f3cad5bed
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-03-24 13:21:52 -0300

    Solved with little-fermat-theorem and congruence basics.

---
+++ cses/mathematics/exponentiation-ii.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define RAYA cerr << "===============================" << endl
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+ll be(ll x, ll y, ll m) {
+  if (y == 0) return 1;
+  ll p = be(x,y/2,m) %m;
+  p = (p * p) % m;
+  return (y%2 == 0) ? p : (x * p) % m;
+}
+
+int main(){
+  FIN;
+
+  int t; cin >> t;
+  while (t--) {
+    ll a,b,c;
+    cin >> a >> b >> c;
+    ll x = be(b,c,MOD-1);
+    ll ans = be(a,x,MOD);
+    cout << ans << '\n';
+  }
+  
+  return 0;
+}
