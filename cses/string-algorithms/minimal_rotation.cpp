8f49f876165732a70b1d7448910e686a4d3788b4
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-07-26 00:44:57 -0300

    Solved with Booth's algorithm.

--- cses/string-algorithms/minimal_rotation.cpp
+++ cses/string-algorithms/minimal_rotation.cpp
@@ -1,86 +1,38 @@
-#include <bits/stdc++.h>
-using namespace std;
-//freopen("input.txt", "r", stdin);
-//freopen("output.txt", "w", stdout);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
-// neal Debugger
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cerr << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
-
-#ifdef LOCAL
-#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-typedef long long ll;
-#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
-#define forn(i, n) forr(i, 0, n)
-#define pb push_back
-#define mp make_pair
-#define all(c) (c).begin(),(c).end()
-#define DBG(x) cerr << #x << " = " << (x) << endl
-#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
-#define esta(x,c) ((c).find(x) != (c).end())
-#define RAYA cerr << "===============================" << endl
-const ll MOD = (ll)(1e9+7); // 998244353 
-const ll INF = (ll)(1<<30); // (1LL<<60)
-const int MAXN = (int)(2e5+5);
-
-#define RB(x) (x<n?r[x]:0)
-void csort(vector<int>& sa, vector<int>& r, int k){
-  int n=sa.size();
-  vector<int> f(max(255,n),0),t(n);
-  forr(i,0,n)f[RB(i+k)]++;
-  int sum=0;
-  forr(i,0,max(255,n))f[i]=(sum+=f[i])-f[i];
-  forr(i,0,n)t[f[RB(sa[i]+k)]++]=sa[i];
-  sa=t;
-}
-
-vector<int> suffix_array(string& s){ // O(n logn)
-  s += '{';
-  int n = s.size(), rank;
-  dbg(n);
-  vector<int> sa(n),r(n),t(n);
-  forr(i,0,n) sa[i] = i, r[i] = s[i];
-  for(int k=1; k<n; k*=2){
-    csort(sa,r,k); csort(sa,r,0);
-    t[sa[0]] = rank=0;
-    forr(i,1,n){
-      if(r[sa[i]] != r[sa[i-1]] || RB(sa[i]+k) != RB(sa[i-1]+k)) rank++;
-      t[sa[i]] = rank;
-    }
-    r = t;
-    if (r[sa[n-1]] == n-1) break;
-  }
-  return sa;
-}
-
-int main(){
-  FIN;
-
-  string s; cin >> s;
+// Function to find the lexicographically minimal rotation using Booth's Algorithm
+int minimal_rotation(const std::string &s) {
+  std::string doubled = s + s;
   int n = s.size();
-  s += s;
-  vector<int> ans = suffix_array(s);
-  int i = ans[0];
+  std::vector<int> f(2 * n, -1); // Failure function
+  int k = 0; // Least rotation of string found so far
 
-  dbg(s);
-  dbg(ans);
-  dbg(i);
-
-  string z;
-  while (n > 0) {
-    if (s[i] != '{') z += s[i], n--;
-    (++i) %= s.size();
+  for (int j = 1; j < 2 * n; ++j) {
+    char sj = doubled[j];
+    int i = f[j - k - 1];
+    while (i != -1 && sj != doubled[k + i + 1]) {
+      if (sj < doubled[k + i + 1]) k = j - i - 1;
+      i = f[i];
+    }
+    if (sj != doubled[k + i + 1]) {
+      if (sj < doubled[k]) k = j;
+      f[j - k] = -1;
+    } else {
+      f[j - k] = i + 1;
+    }
   }
-  cout << z << '\n';
 
+  return k;
+}
 
+int main() {
+  std::string s;
+  std::cin >> s;
+  int rotation_index = minimal_rotation(s);
+  std::string result = s.substr(rotation_index) + s.substr(0, rotation_index);
+  std::cout << result << std::endl;
   return 0;
 }
