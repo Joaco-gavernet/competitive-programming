d905e7691dd73b4ab03efdf53e9ac55c7f2473a3
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-08-08 19:40:03 -0300

    Upsolved problem using modular-arithmetic, number-theory, binary-exponentiation, and combinatorics.

---
+++ rounds/round-952/G.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef pair<ll,ll> pi;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define fs first
+#define ss second
+#define RAYA cerr << "===============================" << endl
+
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+ll be_it(ll b, ll e, ll m) {
+  ll r=1; b%=m;
+  while(e){if(e&1LL)r=r*b%m;b=b*b%m;e/=2;}
+  return r;
+}
+
+void solve() {
+  ll l,r,k; cin >> l >> r >> k;
+
+  ll ansr = be_it(floor(9/k) +1, r, MOD) -1;
+  ll ansl = be_it(floor(9/k) +1, l, MOD) -1;
+
+  ansr -= ansl;
+  if (ansr < 0) ansr += MOD;
+  cout << ansr << '\n';
+}
+
+int main(){
+  FIN;
+
+  int t; cin >> t;
+  while (t--) solve();
+
+  return 0;
+}
