2043646ac972e747da1a5061661f6971f22be6ae
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-08-08 10:44:48 -0300

    Solved with binomial-coefficients from notebook.

--- rounds/round-964/G1.cpp
+++ rounds/round-964/G1.cpp
@@ -20,7 +20,7 @@ typedef long long ll;
 typedef pair<ll,ll> pi;
 typedef vector<bool> vb;
 typedef vector<ll> vi;
-#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define FIN ios::sync_with_stdio(0);cin.tie(0)
 #define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
 #define forn(i, n) forr(i, 0, n)
 #define pb push_back
@@ -32,67 +32,28 @@ typedef vector<ll> vi;
 #define RAYA cerr << "===============================" << endl
 const ll MOD = (ll)(1e9+7); // 998244353 
 const ll INF = (ll)(1<<30); // (1LL<<60)
-const int MAXN = (int)(2e5+5);
+const int MAXN = 1000;
 
-//~ Solucion O(N + Log N)
-vector<ll> fact, ifact;
-
-ll be(ll b, ll e, ll m) {
-  ll r=1; b%=m;
-  while(e){if(e&1LL)r=r*b%m;b=b*b%m;e/=2;}
-  return r;
-}
-ll inv_mod(ll x, ll m){ return be(x,m-2,m); }
-
-void init_fact(int maxn){
-  fact.resize(maxn), ifact.resize(maxn);
-  fact[0] = 1; forr(i, 1, maxn) fact[i] = (fact[i-1]*i)%MOD;
-  ifact[maxn-1] = inv_mod(fact[maxn-1], MOD);
-  for(int i = maxn-2; i >= 0; i--) ifact[i] = (ifact[i+1]*(i+1))%MOD;
-}
-
-ll binom(ll n, ll k){
-  if(n < 0 or k < 0 or n < k) return 0;
-  ll ans = fact[n];
-  ans *= ifact[k]; ans %= MOD;
-  ans *= ifact[n-k]; ans %= MOD;
-  return ans;
-}
 
 int main(){
   FIN;
-
-  // precomputing factorials with dp
-  init_fact(MAXN);
   
   int t; cin >> t;
   while (t--) {
-    int n,k; cin >> n >> k;
-    int on = 0, off = 0;
+    int l = 1, r = MAXN;
+    while (l < r-1) {
+      int mid = l + (r-l)/2;
+      cout << "? " << l << " " << mid << endl;
+      // cout.flush();
+      int ans; cin >> ans;
 
-    vi v(n); 
-    forn(i,n) {
-      cin >> v[i];
-      if (v[i]) on++;
-      else off++;
+      if (ans > l*mid) r = mid;
+      else l = mid;
     }
-
-
-    ll ans = 0;
-    forr(d, k/2 +1, min(on+1,k+1)) {
-      if (k -d > off or d > on) continue;
-
-      // binomial-coefficients
-      ll a = binom(on,d);
-      ll b = binom(off,k-d);
-
-      ans += (a * b) %MOD; 
-      if (ans >= MOD) ans -= MOD;
-    }
-
-    cout << ans << endl;
+    
+    cout << r << endl;
   }
-
-
+  
+  
   return 0;
 }
