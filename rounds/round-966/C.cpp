f47564db0f263ac4521b015e22faa2b4c9845861
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-08-13 13:44:06 -0300

    Solved during official div.

---
+++ rounds/round-966/C.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef pair<ll,ll> pi;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define fs first
+#define ss second
+#define RAYA cerr << "===============================" << endl
+
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+// For Structure 
+struct Node {
+  char val = '0';
+};
+
+void solve() {
+  int n; cin >> n;
+  vi v(n); forn(i,n) cin >> v[i];
+
+  int m; cin >> m;
+  while (m--) {
+    string s; cin >> s;
+    if (s.size() > n or s.size() < n) { cout << "NO\n"; continue; }
+    string ans = "YES\n";
+
+    map<int,Node> mp; 
+    vi mpc(26,INF);
+
+    forn(i,s.size()) {
+      char c = s[i];
+      // dbg(c, v[i], mp[v[i]].val);
+
+      if (mp[v[i]].val == '0' and mpc[c -'a'] == INF) {
+        mp[v[i]].val = c;
+        mpc[c -'a'] = v[i];
+      } else if (mp[v[i]].val != c or mpc[c -'a'] != v[i]) {
+        ans = "NO\n"; 
+        break;
+      } 
+    }
+    cout << ans;
+  }
+  // RAYA;
+}
+
+int main(){
+  FIN;
+
+  int t = 1; cin >> t;
+  while (t--) solve();
+
+
+  return 0;
+}
