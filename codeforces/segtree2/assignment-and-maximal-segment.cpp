2b4726e0c4038b643743c7ac038a116e339d39da
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-09-10 19:06:35 -0300

    Solved after finding missing detail.

--- codeforces/segtree2/assignment-and-maximal-segment.cpp
+++ codeforces/segtree2/assignment-and-maximal-segment.cpp
@@ -38,13 +38,22 @@ const int INF = (1<<30) -1; // (1LL<<60)
 const int MAXN = (int)(2e5+5);
 
 typedef long long tipo;
-const int NEUT = 0; // REMINDER !!! 
+const int NEUT = 0; 
 
 struct node {
   tipo sum, pref, suf, seg, l, r, lazy;
   bool upd;
-  node() { sum = pref = suf = seg = lazy = 0; upd = false; l = r = -1; } // REMINDER !!! SET NEUT
-  node(tipo val, int pos) : sum(val), pref(val), suf(val), seg(val), l(pos), r(pos), lazy(0), upd(false) {} // Set node
+  node() { 
+    sum = pref = suf = seg = lazy = 0; 
+    upd = false; 
+    l = r = -1; 
+  } 
+  node(tipo val, int pos) {
+    sum = pref = suf = seg = val;
+    l = r = pos;
+    lazy = 0;
+    upd = false;
+  }   
   void set_lazy(tipo x) { 
     lazy = x;
     upd = true; 
@@ -55,32 +64,28 @@ struct segtree_lazy {
   #define l(x) int(x<<1)
   #define r(x) int(x<<1|1)
   vector <node> t; int tam;
-  ll n;
 
   node op(node a, node b) { // Operacion de query
-    node aux; 
+    node aux;
     aux.sum = a.sum + b.sum;
-    aux.suf = max(b.suf, a.suf + b.sum);
+
+    aux.suf = max(a.suf + b.sum, b.suf);
     aux.pref = max(a.pref, a.sum + b.pref);
 
     aux.seg = max(a.seg, b.seg);
     aux.seg = max(aux.seg, aux.sum);
     aux.seg = max(aux.seg, aux.suf);
     aux.seg = max(aux.seg, aux.pref);
-    aux.seg = max(aux.seg, 0ll);
-    aux.pref = max(aux.pref, 0ll);
-    aux.suf = max(aux.suf, 0ll);
+    aux.seg = max(aux.seg, a.suf + b.pref);
 
     aux.l = a.l; aux.r = b.r;
     return aux;
   }
 
   void node_update(node &cur) { //Operacion update
-    if (cur.upd) {
-      cur.sum = cur.lazy *(cur.r -cur.l +1);
-      cur.pref = cur.suf = cur.seg = 0;
-      if (cur.lazy > 0) cur.pref = cur.suf = cur.seg = cur.sum;
-    }
+    cur.sum = cur.lazy *(cur.r -cur.l +1);
+    cur.pref = cur.suf = cur.seg = 0;
+    if (cur.lazy > 0) cur.pref = cur.suf = cur.seg = cur.sum;
   }
 
   void reset_lazy(node &cur) {
@@ -116,9 +121,8 @@ struct segtree_lazy {
     cur = op(t[l(p)], t[r(p)]);
   }
 
-  void build(vector <tipo> v, int _n) { // iterative build
-    n = _n;
-    tam = sizeof(int) * 8 - __builtin_clz(_n); tam = 1<<tam;
+  void build(vector <tipo> v, int n) { // iterative build
+    tam = sizeof(int) * 8 - __builtin_clz(n); tam = 1<<tam;
     t.resize(2*tam); v.resize(tam);
     forn(i,tam) t[tam+i] = node(v[i],i);
     for(int i = tam - 1; i > 0; i--) t[i] = op(t[l(i)],t[r(i)]); 
@@ -135,16 +139,16 @@ void solve() {
     ll l, r; cin >> l >> r;
     r--;
 
-    int v; cin >> v;
+    ll v; cin >> v;
     s.update(l,r,v);
 
-    ll tot = s.query(0,n-1).seg;
+    auto ans = s.query(0,n-1);
+    ll tot = 0;
+    tot = max(tot, ans.seg);
+    tot = max(tot, ans.pref);
+    tot = max(tot, ans.suf);
+    tot = max(tot, ans.sum);
     cout << tot << '\n';
-
-    /*
-    forn(i,n) dbg(s.query(i,i).sum);
-    RAYA;
-    */
   }
 }
 
