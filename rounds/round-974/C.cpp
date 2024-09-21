ea65fb17b8ebef66ccc7167e49e5e071fdb8cab4
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-09-21 14:07:48 -0300

    Solved during contest. Keep training to improve.

---
+++ rounds/round-974/C.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define mp make_pair
+#define all(c) (c).begin(),(c).end()
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define esta(x,c) ((c).find(x) != (c).end())
+#define fs first
+#define ss second
+#define RAYA cerr << "===============================" << endl
+
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+const double EPS = (1e-5);
+
+
+void solve() {
+  int n; cin >> n;
+  vi v(n); forn(i,n) cin >> v[i];
+  sort(all(v));
+
+  if (n <= 2) {
+    cout << "-1\n";
+    return;
+  }
+
+  ll sum = 0;
+  forn(i,n) sum += v[i];
+  int pos = n/2;
+  // dbg(n, v);
+  // dbg(pos, sum);
+
+  ll x = -1;
+  x = v[pos]*2*n -sum +1;
+  cout << max(x, 0LL) << '\n';
+
+  // RAYA;
+}
+
+
+int main(){
+  FIN;
+  int t = 1; 
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
