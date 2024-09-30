42af205d42fbd6baef1c557c3da0471dd5453ec2
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-09-29 23:57:50 -0300

    Solved using pragma hack. Don't know how it works internally yet.

---
+++ cses/advanced-techniques/beautiful-subgrids.cpp
@@ -0,0 +1,67 @@
+#pragma GCC target("popcnt")
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define fst first
+#define snd second
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define RAYA cerr << "===============================" << endl
+
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+
+void solve() {
+  int n; cin >> n;
+  vector<bitset<3000>> v;
+  forn(i,n) {
+    string s; cin >> s;
+    v.pb(bitset<3000>(s));
+  }
+  // RAYA; 
+
+  ll tot = 0;
+  forn(i,n) {
+    forr(j,i+1,n) {
+      ll aux = (v[i]&v[j]).count();
+      tot += aux*(aux-1)/2;
+    }
+  }
+
+  cout << tot << '\n';
+}
+
+
+int main(){
+  FIN;
+  int t = 1; 
+  // cin >> t;
+  while (t--) solve();
+  return 0;
+}
