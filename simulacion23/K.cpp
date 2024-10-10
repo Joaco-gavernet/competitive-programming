f2a93643a06e8ebc0aec158678b138148a9570b2
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-10-10 12:11:00 -0300

    Upsolved after codeforces turning-down during the simulation.

--- simulacion23/K.cpp
+++ simulacion23/K.cpp
@@ -14,9 +14,31 @@ typedef pair<ll,ll> ii;
 #define RAYA cerr << "==============================\n"
 
 
-const int MAXN = 1e5+5;
+const int MAXN = 1e6+5;
 const int INF = -1;
 const int MOD = -1;
+
+
+void check(string &s, vi &ans) {
+  int parent = -1; 
+  string aux; 
+  for (char c: s) {
+    if (c == ' ') continue; 
+    if (c != '(' and c != ')') aux += c; 
+    else if (c == '(') {
+      if (SZ(aux)) {
+        ans[stoi(aux)] = parent; 
+        parent = stoi(aux); 
+      }
+      aux = ""; 
+    } else {
+      if (SZ(aux)) ans[stoi(aux)] = parent; 
+      else parent = ans[parent]; 
+      aux = ""; 
+    }
+  }
+  if (SZ(aux)) ans[stoi(aux)] = parent; 
+}
 
 int main() {
   FIN; 
@@ -25,55 +47,17 @@ int main() {
   getline(cin, first); 
   getline(cin, second); 
 
-  // limpio los espacios en las strings
-  string nfirst, nsecond; 
-  for (char c : first) {
-    if (c == ' ') continue; 
-    nfirst += c; 
-  }
-  for (char c : second) {
-    if (c == ' ') continue; 
-    nsecond += c; 
-  }
+  vi fparent(MAXN, 0), sparent(MAXN, 0); 
+  check(first, fparent); 
+  check(second, sparent); 
 
-  vi fparent(MAXN), sparent(MAXN); 
-  int parent = -1; 
-  string aux; 
-  for (char c: nfirst) {
-    if (c != '(' and c != ')') {
-      aux += c; 
-    } else if (c == '(') {
-      if (SZ(aux)) fparent[stoi(aux)] = parent; 
-      if (SZ(aux)) parent = stoi(aux); 
-      aux = ""; 
-    } else {
-      if (SZ(aux)) fparent[stoi(aux)] = parent; 
-      else parent = fparent[parent]; 
-      aux = ""; 
-    }
-  }
-  parent = -1; 
-  aux = ""; 
-  for (char c: nsecond) {
-    if (c != '(' and c != ')') {
-      aux += c; 
-    } else if (c == '(') {
-      if (SZ(aux)) sparent[stoi(aux)] = parent; 
-      if (SZ(aux)) parent = stoi(aux); 
-      aux = ""; 
-    } else {
-      if (SZ(aux)) sparent[stoi(aux)] = parent; 
-      else parent = sparent[parent]; 
-      aux = ""; 
-    }
-  }
   forn(i,MAXN) {
     if (fparent[i] != sparent[i]) {
       cout << "No\n"; 
       return 0;
     }
   }
+
   cout << "Yes\n"; 
-  
   return 0;
 }
