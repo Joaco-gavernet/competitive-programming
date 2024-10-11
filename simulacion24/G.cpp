dcae3cd8d5836c97f2dd077795dac972adecda73
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-10-11 12:14:26 -0300

    Solved with min_prime optimization in find_divisors.

--- simulacion24/G.cpp
+++ simulacion24/G.cpp
@@ -38,16 +38,15 @@ vi criba(ll n) {
 
 vi find_divisors(ll n, vi &primos) {
   vector <pair<ll,ll>> factor; 
-  for(ll prime : primos) {
-    int cont = 0;
-    while(n % prime == 0) {
-      cont++;
-      n /= prime;
+  while (n > 1) {
+    ll prime = min_prime[n]; 
+    int cont = 0; 
+    while (n %prime == 0) {
+      cont++; 
+      n /= prime; 
     }
-    if(cont > 0) factor.pb({prime,cont});
+    factor.pb({prime,cont}); 
   }
-
-  if(n > 1) factor.pb({n,1});
 
   vi divisores = {1};
   for(auto [p,exp] : factor) {
