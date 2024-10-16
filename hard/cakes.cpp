e15f6e8b2fad5f30618e72c59be161b10e448c45
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-10-16 00:26:53 -0300

    Last update. WA test 7.

--- hard/cakes.cpp
+++ hard/cakes.cpp
@@ -33,51 +33,59 @@ typedef vector<ll> vi;
 #define RAYA cerr << "===============================" << endl
 
 const ll MOD = (ll)(1e9+7); // 998244353 
-const ll INF = (ll)(1<<30); // (1LL<<60)
+const ll INF = (ll)(1LL<<60); // (1LL<<60)
 const int MAXN = (int)(2e5+5);
 
-/* Algoritmo O(n^3) para assignment problem
- * Inspirando el el notebook vasito, quien se inspiro
- * en http://e-maxx.ru/algo/assignment_hungary
- */
-typedef long double td; typedef vector<td> vd;
-const td INF=1e100;//for maximum set INF to 0, and negate costs
-bool zero(td x){return fabs(x)<1e-9;}//change to x==0, for ints/ll
-struct Hungarian{
-  int n; vector<vd> cs; vi L, R;
-  Hungarian(int N, int M):n(max(N,M)),cs(n,vd(n)),L(n),R(n){
-    forn(x,N)forn(y,M)cs[x][y]=INF;
+// Dinic: Max Flow en O(V^2 E). Para el grafo bipartito con source
+// y sink dummy, funciona en O(sqrt(V) E). Equivalente a Hopcroft-Karp.
+
+// Matching: aristas saturadas (que no incluyan source/sink)
+// Min cut: nodos con dist>=0 vs nodos con dist<0
+// MVC: Nodos izquierda con dist<0 + nodos derecha con dist>0
+// Maximum Independent Set: complemento de MVC (N-MVC)
+struct Dinic{
+  int nodes,src,dst;
+  vector<int> dist,q,work;
+  struct edge { int to,rev; ll f,cap; };
+  vector<vector<edge>> g;
+  Dinic(int x):nodes(x),dist(x),q(x),work(x),g(x){}
+  void add_edge(int s, int t, ll cap){
+    g[s].pb({t,SZ(g[t]),0,cap});
+    g[t].pb({s,SZ(g[s])-1,0,0});
   }
-  void set(int x,int y,td c){cs[x][y]=c;}
-  td assign() {
-    int mat = 0; vd ds(n), u(n), v(n); vi dad(n), sn(n);
-    forn(i,n)u[i]=*min_element(all(cs[i]));
-    forn(j,n){v[j]=cs[0][j]-u[0];forr(i,1,n)v[j]=min(v[j],cs[i][j]-u[i]);}
-    L=R=vi(n, -1);
-    forn(i,n)forn(j,n)
-    if(R[j]==-1&&zero(cs[i][j]-u[i]-v[j])){L[i]=j;R[j]=i;mat++;break;}
-    for(;mat<n;mat++){
-      int s=0, j=0, i;
-      while(L[s] != -1)s++;
-      fill(all(dad),-1);fill(all(sn),0);
-      forn(k,n)ds[k]=cs[s][k]-u[s]-v[k];
-      for(;;){
-        j = -1;
-        forn(k,n)if(!sn[k]&&(j==-1||ds[k]<ds[j]))j=k;
-        sn[j] = 1; i = R[j];
-        if(i == -1) break;
-        forn(k,n)if(!sn[k]){
-          auto new_ds=ds[j]+cs[i][k]-u[i]-v[k];
-          if(ds[k] > new_ds){ds[k]=new_ds;dad[k]=j;}
-        }
+  bool dinic_bfs(){
+    fill(all(dist),-1);dist[src]=0;
+    int qt=0;q[qt++]=src;
+    forn(qh, qt){
+      int u=q[qh];
+      forn(i,SZ(g[u])){
+        edge &e=g[u][i];int v=g[u][i].to;
+        if(dist[v]<0&&e.f<e.cap)dist[v]=dist[u]+1,q[qt++]=v;
       }
-      forn(k,n)if(k!=j&&sn[k]){auto w=ds[k]-ds[j];v[k]+=w,u[R[k]]-=w;}
-      u[s] += ds[j];
-      while(dad[j]>=0){int d = dad[j];R[j]=R[d];L[R[j]]=j;j=d;}
-      R[j]=s;L[s]=j;
     }
-    td value=0;forn(i,n)value+=cs[i][L[i]];
-    return value;
+    return dist[dst]>=0;
+  }
+  ll dinic_dfs(int u, ll f){
+    if(u==dst)return f;
+    for(int &i=work[u];i<SZ(g[u]);i++){
+      edge &e=g[u][i];
+      if(e.cap<=e.f)continue;
+      int v=e.to;
+      if(dist[v]==dist[u]+1){
+        ll df=dinic_dfs(v,min(f,e.cap-e.f));
+        if(df>0){e.f+=df;g[v][e.rev].f-=df;return df;}
+      }
+    }
+    return 0;
+  }
+  ll max_flow(int _src, int _dst){
+    src=_src;dst=_dst;
+    ll result=0;
+    while(dinic_bfs()){
+      fill(all(work),0);
+      while(ll delta=dinic_dfs(src,INF))result+=delta;
+    }
+    return result;
   }
 };
 
@@ -85,16 +93,46 @@ int main(){
   FIN;
 
   int g, c, t; cin >> g >> c >> t; 
-  vi profit(c); forn(i,c) cin >> price[i]; 
+  vi profit(c); forn(i,c) cin >> profit[i]; 
   vi ing(g); forn(i,g) cin >> ing[i]; 
   vi tool(t); forn(i,t) cin >> tool[i]; 
   vector<vi> recipes(c, vi(g)); 
-  forn(i,c) forn(j,g) recipes[i][j]; 
+  forn(i,c) forn(j,g) cin >> recipes[i][j]; 
   vector<vi> tools_needed(c); 
   forn(i,c) { 
     int tot, x; cin >> tot; 
     while (tot--) cin >> x, tools_needed[i].pb(x); 
   } 
+  /*
+  dbg(profit); 
+  dbg(ing); 
+  dbg(tool); 
+  dbg(recipes); 
+  dbg(tools_needed); 
+  */
+
+  // discount the ingredients from the profit
+  forn(i,c) forn(j,g) profit[i] -= recipes[i][j] *ing[j];
+  // dbg(profit); 
+
+  ll ans = 0;
+  forn(i,c) ans += profit[i]; 
+
+  int n = 2 + c + t; 
+  Dinic red(n); 
+  forn(i,c) {
+    // dbg(0, i, profit[i]); 
+    red.add_edge(0, 1+i, profit[i]); 
+    for (int k : tools_needed[i]) red.add_edge(1+i, k + c, INF); 
+  }
+  forn(i,t) red.add_edge(c +1 +i, n-1, tool[i]); 
+
+  ll aux = red.max_flow(0,n-1); 
+  // dbg(ans, aux); 
+  ans -= aux; 
+  if (ans < 0) ans = 0; 
+  cout << ans << '\n'; 
+
 
 
   return 0;
