f8716722e7011162409012c691daeb303c064ba3
Joaquin Gavernet <joaquingavernet@gmail.com>
2024-10-18 20:57:35 -0300

    Solved with burnt ideas, but nice implementation details.

--- hard/cops-and-robers.cpp
+++ hard/cops-and-robers.cpp
@@ -92,50 +92,55 @@ struct Dinic{
 int dx[] = {0,1,0,-1}; 
 int dy[] = {-1,0,1,0}; 
 
+void bfs(const int n, const int m, const int yb, const int xb, const vector<string> &tab, Dinic &din) {
+  vector<vb> visto(m, vb(n,false)); 
+  queue<ii> q; q.push({yb, xb}); 
+  queue<ii> last; last.push({-1, -1}); 
+  visto[yb][xb] = true; 
+
+  while (q.size()) {
+    auto [y, x] = q.front(); q.pop(); 
+    auto [ly, lx] = last.front(); last.pop(); 
+    forn(i,4) {
+      int _y = y + dy[i]; 
+      int _x = x + dx[i];
+      if (_y < 0 or _y >= m or _x < 0 or _x >= n) {
+        din.add_edge(y*n +x +m*n, 2*m*n, INF); 
+      } else {
+        din.add_edge(y*n +x +m*n, _y*n +_x, INF); 
+        if (visto[_y][_x] == false) {
+          visto[_y][_x] = true; 
+          q.push({_y, _x}); 
+          last.push({y,x}); 
+        }
+      }
+    }
+  }
+}
+
 int main(){
   FIN;
 
   int n, m, c; cin >> n >> m >> c; 
-  const int N = n +2; 
-  const int M = m +2; 
-
-  vector<string> tab(M); 
+  vector<string> tab(m); 
   int yb = -1, xb = -1; 
-  forr(i,1,M-1) {
+  forn(i,m) {
     cin >> tab[i]; 
-    tab[i] = '$' + tab[i] + '$'; 
-    forn(j,N) if (tab[i][j] == 'B') yb = i, xb = j; 
+    forn(j,n) if (tab[i][j] == 'B') yb = i, xb = j; 
   }
   vi cost(c); forn(i,c) cin >> cost[i]; 
-  tab[0] = tab[M-1] = string(N,'$'); 
 
-  /*
-  dbg(yb, xb); 
-  forn(i,M) dbg(i, tab[i]); 
-  RAYA; 
-  */ 
-
-  Dinic din((M*N +1)); 
-  forn(y,M) forn(x,N) {
-    if (tab[y][x] == '$') continue; 
-    forn(i,4) {
-      int _y = y + dy[i]; 
-      int _x = x + dx[i];
-      if (_y < 0 or _y > M) continue; 
-      if (_x < 0 or _x > N) continue; 
-      ll _cost = INF; 
-      char c = tab[_y][_x]; 
-      if (c != '.' and c != 'B' and c != '$') _cost = cost[c -'a']; 
-      if (c == '$') din.add_edge(y*N +x, M*N, _cost); 
-      else {
-        // dbg(y*N +x, _y*N +_x, _cost); 
-        din.add_edge(y*N +x, _y*N +_x, _cost); 
-      }
-    }
+  Dinic din(2*m*n +5); 
+  forn(y,m) forn(x,n) {
+    ll _cost = INF; 
+    char c = tab[y][x]; 
+    if (c != '.' and c != 'B' and c != '$') _cost = cost[c -'a']; 
+    din.add_edge(y*n +x, y*n +x +m*n, _cost); 
   }
 
-  // dbg(yb*N +xb); 
-  ll ans = din.max_flow(yb*N +xb, M*N); 
+  bfs(n,m,yb,xb,tab,din); 
+
+  ll ans = din.max_flow(yb*n +xb, 2*m*n); 
   if (ans >= INF) cout << "-1\n"; 
   else cout << ans << '\n'; 
 
