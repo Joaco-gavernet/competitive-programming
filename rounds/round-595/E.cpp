3861b4eccef55408d800dd42f7c626ee8de58503
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-01-13 23:53:27 -0300

    Solved during simulation. Upsolved C1 & C2.

---
+++ rounds/round-595/E.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+ 
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define fst first
+#define snd second
+#define SZ(x) int((x).size()) 
+#define DBG(x) cerr << #x << " = " << (x) << endl
+#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
+#define RAYA cerr << "===============================" << endl
+
+const ll MOD = (ll)(1e9+7); // 998244353 
+const ll INF = (ll)(1<<30); // (1LL<<60)
+const int MAXN = (int)(2e5+5);
+
+
+
+
+int main(){
+  FIN;
+
+  int n, c; cin >> n >> c; 
+  vi a(n); forr(i,1,n) cin >> a[i]; 
+  vi b(n); forr(i,1,n) cin >> b[i]; 
+
+  vector<vi> dp(n, {0,0}); 
+  dp[1][0] = a[1]; 
+  dp[1][1] = c +b[1]; 
+
+  forr(i,2,n) {
+    dp[i][0] = min(dp[i-1][0], dp[i-1][1]) +a[i]; 
+    dp[i][1] = min(dp[i-1][0] +c, dp[i-1][1]) +b[i]; 
+  }
+
+  forn(i,n) cout << min(dp[i][0], dp[i][1]) << ' ';
+  cout << '\n'; 
+
+
+  return 0;
+}
