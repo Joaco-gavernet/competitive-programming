520763dff3a21ee51593af768dd9dd00877845b7
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-02-08 00:38:32 -0300

    Keep upsolving python stuff.

--- rounds/round-173/second/C.py
+++ rounds/round-173/second/C.py
@@ -6,8 +6,7 @@ import math
 
 def checks(pref, inicio, fin):
     inicio += 1; fin += 1
-    print("inicio y fin", inicio, fin) 
-    l = ax = inicio -1; r = inicio
+    l = inicio -1; r = inicio; ax = 0
     while r < fin: 
         ax = max(ax, pref[r] -pref[l]) 
         if pref[r] -pref[l] <= 0:
@@ -16,7 +15,6 @@ def checks(pref, inicio, fin):
 
     l = inicio -1; r = inicio; an = 0
     while r < fin: 
-        print("in", l, r) 
         an = min(an, pref[r] -pref[l]) 
         if pref[r] -pref[l] >= 0:
             l = r 
@@ -30,7 +28,7 @@ for _ in range(int(input())):
 
     idx = -1 
     for i in range(n): 
-        if a[i] != 1: 
+        if abs(a[i]) != 1: 
             idx = i 
 
     # two-pointers to find best max and min in left interval [0, idx] 
@@ -38,25 +36,39 @@ for _ in range(int(input())):
     gn = gx = 0
     for x in a:
         pref.append(pref[-1] + x) 
-    print(pref) 
 
-    print("idx", idx) 
     if idx != -1: 
         an, ax = checks(pref, 0, idx) 
         bn, bx = checks(pref, idx +1, n) 
-        print("deb", bn, bx) 
         gn = min(an, bn); gx = max(ax, bx)  
     else: 
         gn, gx = checks(pref, 0, n) 
 
-    ans = [] 
+    ans = set()  
     for x in range(gn, gx +1):
-        ans.append(x) 
-    print("ans", ans) 
+        ans.add(x) 
+        
+    # second part considering idx != -1
+    if idx != -1: 
+        an = ax = 0
+        i = idx -1
+        x = 0 
+        while i >= 0: 
+            x += a[i] 
+            an = min(an, x) 
+            ax = max(ax, x) 
+            i -= 1 
 
-    print("====================================") 
-
-
-    # two-pointers to find best max and min in right interval [idx, n] 
-    # if idx != -1: 
-
+        bn = bx = 0
+        i = idx +1
+        x = 0 
+        while i < n: 
+            x += a[i] 
+            bn = min(bn, x) 
+            bx = max(bx, x) 
+            i += 1 
+        gn = an + bn; gx = ax + bx 
+        for x in range(gn, gx +1): 
+            ans.add(a[idx] +x) 
+    print(len(ans)) 
+    print(*sorted(ans)) 
