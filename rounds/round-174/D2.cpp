6ba4736f8db33e48528bd926fe960ea8af376cee
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-02-19 17:29:50 -0300

    Upsolved problem D. Avoid editorials.

--- rounds/round-174/D2.cpp
+++ rounds/round-174/D2.cpp
@@ -26,59 +26,37 @@ typedef vector<ll> vi;
 #define RAYA cerr << "===============================" << endl
 
 
+int check(int l, string s, int balanced, vi c) {
+  int b = balanced; 
+  const int n = SZ(s); 
+  int r = l; 
+  for (; r < n and (b or (r < n/2 and s[r] != s[n-1-r])); r++) {
+    if (r < n/2 and c[s[r] -'a'] > 0) c[s[r] -'a'] -= 2, b--;
+    else if (r >= n/2 and c[s[r] -'a'] < 0) c[s[r] -'a'] += 2, b--;
+  }
+  if (balanced) r = max(r, n/2 +balanced/2); 
+  return r -l; 
+} 
+
 void solve() {
   string s; cin >> s; 
   const int n = SZ(s); 
-  map<char, vi> l, r; 
-  
-  // preproc maps
-  for (char c = 'a'; c != '{'; c++) {
-    vi v(n, -1); 
-    if (s[0] == c) v[0] = 0;
-    forr(i,1,n) {
-      if (s[i] == c) v[i] = 0; 
-      else if (v[i-1] == -1) v[i] = -1; 
-      else v[i] = v[i-1] +1; 
-    } 
-    l[c] = v; 
-    vi u(n, -1); 
-    if (s[n-1] == c) u[n-1] = 0; 
-    for (int i = n-2; i >= 0; i--) {
-      if (s[i] == c) u[i] = 0; 
-      else if (u[i+1] == -1) u[i] = -1; 
-      else u[i] = u[i+1] +1; 
-    } 
-    r[c] = u;
-    // dbg(c, u); 
-  } 
 
-  int il = -1, ir = -1; 
-  int dl = -1, dr = -1; 
-  vb fix(n, false); 
-  ll ans = 0; 
-  forn(i,n/2) if (s[i] != s[n-1-i]) {
-    if (!fix[i] and !fix[n-1-i]) {
-      int x = r[s[n-1-i]][i];
-      int y = l[s[i]][n-1-i];
-      if (x < y) {
-        if (p != -1) x += i-p;
-        if (x +1 > ans) {
-          if (p == -1) p = i; 
-          fix[i +x] = true; 
-          ans = x +1; 
-        } 
-      } else {
-        if (p != -1) y += i-p;
-        if (y +1 > ans) {
-          if (p == -1) p = i; 
-          fix[n -1 -i -y] = true; 
-          ans = y +1; 
-        } 
-      } 
-    } 
-  }
+  // check if balanced string 
+  vi c(30, 0); 
+  forn(i,n/2) c[s[i] -'a']++; 
+  forn(i,n/2) c[s[i +n/2] -'a']--; 
+  int balanced = 0; 
+  forn(i,30) if (c[i] != 0) c[i], balanced += abs(c[i])/2; 
+
+  int i = 0; 
+  for (;i < n and s[i] == s[n-1-i]; i++) {} 
+  int ans = check(i, s, balanced, c); 
+  reverse(all(s)); 
+  for(auto &x: c) x *= -1;
+  ans = min(ans, check(i, s, balanced, c)); 
   cout << ans << '\n'; 
-}
+} 
 
 
 int main(){
