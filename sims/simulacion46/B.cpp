d7d39173003ce971e9529a3d130786ec4a3502cb
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-02-26 19:21:11 -0300

    Upsolved problem B with greedy approach.

--- sims/simulacion46/B.cpp
+++ sims/simulacion46/B.cpp
@@ -12,16 +12,54 @@ typedef pair<ll,ll> ii;
 #define SZ(x) int((x).size()) 
 #define DBG(x) cerr << #x << " = " << x << endl
 #define RAYA cerr << "===============================\n" 
+#define pb push_back
 
 
+void check(int &best, int &fs, int &ss, map<int, vi> &r, int mx) {
+  for (auto [k, st]: r) {
+    sort(all(st)); 
+    if (SZ(st) < best or SZ(st) == mx) continue; 
+
+    forn(i, SZ(st)) {
+      if (i +1 != st[i]) {
+        if (best < SZ(st) -i) {
+          best = SZ(st) -i; 
+          fs = k; 
+          ss = i +1;
+        } 
+        break; 
+      } 
+    } 
+
+    int i = 0; 
+    while (st[SZ(st) -1 -i] == mx -i) i++; 
+    if (best < SZ(st) -i) {
+      best = SZ(st) -i; 
+      fs = k; 
+      ss = mx -i;
+    } 
+  } 
+} 
 
 int main() {
   FIN; 
 
   int n, m, k; cin >> n >> m >> k; 
-  vector<ii> pares(k); forn(i,k) cin >> pares[i].first >> pares[i].second; 
 
-  
+  map<int, vi> rx, ry; 
+  forn(i,k) {
+    int x, y; cin >> x >> y; 
+    rx[x].pb(y); 
+    ry[y].pb(x); 
+  } 
+
+  int best = 0;
+  int ax = 0, ay = 0; 
+
+  check(best, ay, ax, ry, n); 
+  check(best, ax, ay, rx, m); 
+  cout << best << '\n'; 
+  cout << ax << ' ' << ay << '\n'; 
 
   return 0;
 }
