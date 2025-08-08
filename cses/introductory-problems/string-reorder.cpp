bb35392ecef31b5f63c897aa09b5d48d1604b3c9
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-08-08 12:00:03 -0300

    Finally solved implementation problem.

--- cses/introductory-problems/string-reorder.cpp
+++ cses/introductory-problems/string-reorder.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //freopen("input.txt", "r", stdin);
 //freopen("output.txt", "w", stdout);
-
+ 
 // neal Debugger
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
 template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
@@ -10,7 +10,7 @@ template<typename T_container, typename T = typename enable_if<!is_same<T_contai
 void dbg_out() { cerr << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-
+ 
 typedef long long ll;
 typedef pair<ll,ll> ii;
 typedef vector<bool> vb;
@@ -24,51 +24,62 @@ typedef vector<ll> vi;
 #define ss second
 #define SZ(x) int((x).size()) 
 #define RAYA cerr << "===============================" << endl
-
-
+ 
+ 
 int main(){
   FIN;
-
+ 
   string s; cin >> s; 
+
   const int n = SZ(s); 
   map<char,int> mp; 
   for (auto c: s) {
-      mp[c]++; 
-      if (mp[c] > n/2 +(n %2)) {
-          cout << "-1\n";
-          return 0; 
-      }    
+    mp[c]++; 
+    if (mp[c] > n/2 +(n %2)) {
+      cout << "-1\n";
+      return 0; 
+    }    
   }
   vector<pair<char,int>> prs; 
   for (auto [k, v]: mp) prs.pb({k, v}); 
   sort(all(prs)); 
   reverse(all(prs)); 
-  dbg(prs); 
 
   string ans = "";
   pair<char, int> a = prs.back(); prs.pop_back(); 
   pair<char, int> b = {-1, -1}; 
   if (SZ(prs)) b = prs.back(), prs.pop_back(); 
-  dbg(a, b); 
   ans += a.ff, a.ss--; 
   if (a.ss == 0) a = b, b = {-1,-1}; 
   if (b.ff == -1 and SZ(prs)) b = prs.back(), prs.pop_back(); 
   while (b.ss != -1) {
-      dbg(ans); 
-      dbg(a, b); 
-      if (ans.back() == a.ff) {
-          ans += b.ff, b.ss--; 
-          if (b.ss == 0) b = {-1,-1}; 
-          if (b.ff == -1 and SZ(prs)) b = prs.back(), prs.pop_back(); 
-      } else {
-          ans += a.ff, a.ss--; 
-          if (a.ss == 0) a = b, b = {-1,-1}; 
-          if (b.ff == -1 and SZ(prs)) b = prs.back(), prs.pop_back(); 
-      }
+    if (ans.back() == a.ff) {
+      ans += b.ff, b.ss--; 
+      if (b.ss == 0) b = {-1,-1}; 
+      if (b.ff == -1 and SZ(prs)) b = prs.back(), prs.pop_back(); 
+    } else {
+      ans += a.ff, a.ss--; 
+      if (a.ss == 0) a = b, b = {-1,-1}; 
+      if (b.ff == -1 and SZ(prs)) b = prs.back(), prs.pop_back(); 
+    }
   }
-  dbg(a); 
-  cout << ans << '\n'; 
+  if (ans.back() != a.ff) ans += a.ff, a.ss--; 
+  int i = SZ(ans) -1; 
+  while (i > 0 and a.ss) {
+      if (ans[i] != a.ff and ans[i+1] != a.ff) {
+          if (ans[i] > ans[i+1] and a.ss > 1) swap(ans[i], ans[i+1]); 
+          ans.insert(i+1, 1, a.ff), a.ss--; 
+      }
+      i--; 
+  } 
 
+  // sort again each of the parity groups 
+  vector<char> odds; 
+  for (int j = n -2; j >= 1 and ans[j-1] == ans.back(); j -= 2) odds.pb(ans[j]); 
+  sort(all(odds)); 
+  for (int i = n-2; SZ(odds); i -= 2) ans[i] = odds.back(), odds.pop_back(); 
+
+  cout << ans << '\n'; 
 
   return 0;
 }
