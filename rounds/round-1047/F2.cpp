c5f996d7d2f65ae9b8289d4c4ff009062167f3ec
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-11-04 11:03:46 -0300

    Upsolved with simple and clean set.

---
+++ rounds/round-1047/F2.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
+
+
+void solve() {
+  int n; cin >> n; 
+  vi a(n); forn(i,n) cin >> a[i]; 
+  vi b(n); forn(i,n) cin >> b[i]; 
+
+  set<ii> st; 
+  ll tot = 0; 
+  forn(i,n) {
+    ll mx = max(a[i], b[i]); 
+    auto it = st.upper_bound({mx,-1});
+    ll frees = -1; 
+    if (it != st.end()) frees = it->ss; 
+    if (a[i] == b[i]) frees = i; 
+    if (frees > -1) tot += (frees+1)*(n-i); 
+
+    while (SZ(st) and st.begin()->ff <= a[i]) st.erase(st.begin());
+    st.insert({a[i], i}); 
+  }
+
+  cout << tot << '\n'; 
+}
+
+
+int main(){
+  FIN;
+  int t = 1; 
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
