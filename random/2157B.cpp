b4d70198be17266338e435d401643771a318fd25
Joaquin Gavernet <joaquingavernet@gmail.com>
2025-11-30 21:05:25 -0300

    just keep streak.

---
+++ random/2157B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
+
+
+void solve() {
+  int n, x, y; cin >> n >> x >> y; 
+  string s; cin >> s; 
+  x = abs(x), y = abs(y); 
+  int fours = 0, eights = 0; 
+  for (char c: s) fours += (c == '4'); 
+  eights = n - fours; 
+
+  int needx = max(0, x -eights); 
+  int needy = max(0, y -eights); 
+
+  if (needx + needy <= fours) cout << "YES\n"; 
+  else cout << "NO\n";  
+}
+
+
+int main(){
+  FIN;
+  int t = 1; 
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
