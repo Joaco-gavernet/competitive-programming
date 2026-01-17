691701a05a52892e53b144f3038fedce57b5f40a
Joaquin Gavernet <joaquingavernet@gmail.com>
2026-01-17 14:11:10 -0300

    solved during official div2

---
+++ rounds/round-1073/B.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
+
+int mex(map<int,int> &mp, int n) {
+  forn(i,n+1) if (mp[i] == 0) return i;
+  return -1;
+}
+
+void solve() {
+  int n; cin >> n;
+  vi a(n); forn(i,n) cin >> a[i]; 
+  sort(all(a));
+
+  map<int,int> ma, mb;
+  ma[a[0]]++;
+  forr(i,1,n) mb[a[i]]++;
+
+  int mxa = mex(ma, n);
+  int mxb = mex(mb, n);
+  if (mxa == mxb) {
+    cout << "NO\n";
+    return;
+  }
+  forr(i,1,n-1) {
+    ma[a[i]]++;
+    mb[a[i]]--;
+    int mxa = mex(ma, n);
+    int mxb = mex(mb, n);
+    if (mxa == mxb) {
+      cout << "NO\n";
+      return;
+    }
+  }
+  cout << "YES\n";
+}
+
+
+int main(){
+  FIN;
+  int t = 1; 
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
