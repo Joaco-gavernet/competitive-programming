1e2bd5d87c004df0a5cd6f8ddc1dda7a5e6578f7
Joaquin Gavernet <joaquingavernet@gmail.com>
2026-02-06 18:49:33 -0300

    upsolved

---
+++ random/1841B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+//freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
+
+
+void solve() {
+  int n; cin >> n; 
+  vi x(n); forn(i,n) cin >> x[i]; 
+
+  bool op = false; 
+  string ans = "1"; 
+  vi v = {x[0]}; 
+  forr(i,1,n) {
+    if (op == false) {
+      if (v.back() > x[i] and x[i] <= v.front()) {
+        op = true; 
+        ans += '1'; 
+        v.pb(x[i]); 
+      } else if (v.back() <= x[i]) {
+        ans += '1'; 
+        v.pb(x[i]);
+      } else ans += '0'; 
+    } else {
+      if (v.back() <= x[i] and x[i] <= v.front()) ans += '1', v.pb(x[i]); 
+      else ans += '0';  
+    } 
+  } 
+  cout << ans << '\n'; 
+}
+
+
+int main(){
+  FIN;
+  int t = 1; 
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
