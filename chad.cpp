9e73eea07539c8e1084bbb3bd0352b9957b96a2a
Joaquin Gavernet <joaquingavernet@gmail.com>
2026-02-28 13:29:37 -0300

    little diff.

--- chad.cpp
+++ chad.cpp
@@ -8,12 +8,25 @@ template<typename T_container, typename T = typename enable_if<!is_same<T_contai
 void dbg_out() { cerr << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#define NAN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define NaN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
 
 
 
 int main(){
-  NAN;
+  NaN;
 
 
   return 0;
