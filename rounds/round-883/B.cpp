5f9f0eea84b808dd34ccf6a73348e3f760859711
Joaquin Gavernet <joaquingavernet@gmail.com>
2026-03-25 17:34:57 -0300

    upsolve unsolved

--- rounds/round-883/B.cpp
+++ rounds/round-883/B.cpp
@@ -11,10 +11,10 @@ int main() {
 	forn(_,t) {
 		string res = "DRAW";
 		forn(i,3) cin >> v[i];
-		forn(i,3) if (v[i][0] == v[i][1] and v[i][1] == v[i][2]) res = v[i][1];
-		forn(i,3) if (v[0][i] == v[1][i] and v[1][i] == v[2][i]) res = v[1][i];
-		if (v[1][1] == v[0][0] and v[0][0] == v[2][2]) res = v[1][1];			
-		if (v[1][1] == v[0][2] and v[0][2] == v[2][0]) res = v[1][1];			
+		forn(i,3) if (v[i][0] == v[i][1] and v[i][1] == v[i][2] and v[i][1] != '.') res = v[i][1];
+		forn(i,3) if (v[0][i] == v[1][i] and v[1][i] == v[2][i] and v[1][i] != '.') res = v[1][i];
+		if (v[1][1] == v[0][0] and v[0][0] == v[2][2] and v[1][1] != '.') res = v[1][1];			
+		if (v[1][1] == v[0][2] and v[0][2] == v[2][0] and v[1][1] != '.') res = v[1][1];			
 		cout << res << '\n';
 	}
 
