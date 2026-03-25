c6a633b958351349602fe2bf0b6161eeb6e4a489
Joaquin Gavernet <joaquingavernet@gmail.com>
2026-03-25 17:28:46 -0300

    upsolve unsolved.

--- rounds/round-894/plantilla.cpp
+++ rounds/round-894/plantilla.cpp
@@ -4,47 +4,38 @@ using namespace std;
 typedef long long ll;
 #define forr(i,a,b) for(ll i=(a);i<(ll)(b);i++)
 #define forn(i,n) forr(i,0,n)
+#define SZ(x) int((x).size())
 
 #define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #define pb push_back
+#define DBG(x) cerr << #x << " = " << x << endl 
 
 int main() {
   FIN;
 
-  cout << "pre forn\n";  
   ll t; cin >> t;
   ll n,m;
   
   forn(_,t) {
     cin >> n >> m;
-    vector<string> v(m,"");
+    vector<string> tab(n); 
+    vector<map<char,int>> v(m); 
 
     forn(i,n) {
-      string act; cin >> act;
-      cout << "aca\n";
-      forn(j,(ll)act.length()) {
-	v[i].pb(act[j]);
-      }
+      cin >> tab[i]; 
+      forn(j,m) v[j][tab[i][j]]++; 
     }
-    
-    ll k = 0;
-    string x = "vika";
-    
-    forn(i,m) {
-      forn(j,n) {
-	if (x[k] == v[i][j]) {
-	  k++;
-	  continue;
-	} 
-      }
-      
-      if (k==4) break;
-    }
-    
-    if (k == 4) cout << "yes\n";
-    else cout << "no\n";    
+    string check = "vika"; 
+
+    for (int i = m - 1; i >= 0; i--) {
+      if (v[i][check.back()] > 0) check.pop_back(); 
+      if (SZ(check) == 0) break; 
+    } 
+
+    if (SZ(check) == 0) cout << "YES\n";
+    else cout << "NO\n"; 
   }
-  
-  
+
+
   return 0;
 }
