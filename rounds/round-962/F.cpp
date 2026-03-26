c35c5dea4197e00c528cd8b0d04652fd8246fdcd
Joaquin Gavernet <joaquingavernet@gmail.com>
2026-03-26 20:59:42 -0300

    upsolved unsolved with burnt classic bs+brute.

--- rounds/round-962/F.cpp
+++ rounds/round-962/F.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-//freopen("input.txt", "r", stdin);
-//freopen("output.txt", "w", stdout);
 
 // neal Debugger
 template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
@@ -9,84 +7,72 @@ template<typename T_container, typename T = typename enable_if<!is_same<T_contai
  
 void dbg_out() { cerr << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
- 
-#ifdef LOCAL
 #define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
 
 typedef long long ll;
-#define FIN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define NaN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 #define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
 #define forn(i, n) forr(i, 0, n)
 #define pb push_back
-#define mp make_pair
 #define all(c) (c).begin(),(c).end()
-#define DBG(x) cerr << #x << " = " << (x) << endl
-#define DBGV(v,n) forn(i,n) cout << v[i] << " "; cout << endl
-#define esta(x,c) ((c).find(x) != (c).end())
-#define RAYA cerr << "===============================" << endl
-const ll MOD = (ll)(1e9+7); // 998244353 
-const ll INF = (ll)(1<<30); // (1LL<<60)
-const int MAXN = (int)(2e5+5);
-
-typedef pair<int,int> ii;
-#define fs first
+#define ff first
 #define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
 
-
-struct cmp {
-  bool operator() (ii a, ii b) const {
-    if (a.fs > b.fs or (a.fs == b.fs and a.ss < b.ss)) return true;
-    else return false;
-  }
-};
-
-void dbgs(multiset<ii,cmp> s) {
-  for (auto i: s) dbg(i);
-  RAYA;
-}
 
 void solve() {
-  int n,k; cin >> n >> k;
-  vector<int> a(n), b(n);
-  multiset<pair<int,int>,cmp> s;
-  forn(i,n) cin >> a[i];
-  forn(i,n) {
-    cin >> b[i];
-    s.insert({a[i],b[i]});
-  }
-  // dbgs(s);
+  int n, k; cin >> n >> k; 
+  vi a(n); forn(i,n) cin >> a[i]; 
+  vi b(n); forn(i,n) cin >> b[i]; 
 
-  ll ans = 0;
-  while (k--) {
-    int act = (*s.begin()).first;
-    ans += act;
-    auto p = *s.begin();
-    s.erase(s.lower_bound(p));
-    p.first = max(0, p.first -p.second);
-
-    while (k and (p.fs > (*s.begin()).fs or (p.fs == (*s.begin()).fs and p.ss < (*s.begin()).ss))) {
-      ans += p.fs;
-      p.first = max(0, p.first -p.second);
-      k--;
+  auto f = [&](ll x) -> ll {
+    ll tot = 0; 
+    forn(i,n) if (a[i] >= x) {
+      tot += (a[i] - x) / b[i] + 1; 
     }
-    s.insert(p);
-  }
+    return tot; 
+  }; 
 
-  cout << ans << '\n';
+  ll l = -1, r = 1e10; 
+  while (l + 1 < r) {
+    ll mid = l + (r - l) / 2;
+    if (f(mid) <= k) r = mid; 
+    else l = mid; 
+  } 
+  const ll F = f(r); 
+  k -= F; 
+  // assert(k < n); // otherwise we can do another operation on all a[i] 
+
+  ll tot = 0; 
+  forn(i,n) if (a[i] >= r) {
+    ll steps = (a[i] - r) / b[i] + 1; 
+    tot += steps * (a[i] - b[i] * (steps - 1) + a[i]) / 2; 
+    a[i] -= steps * b[i]; 
+    a[i] = max(0LL, a[i]); 
+  } 
+
+  vector<ii> v(n); 
+  forn(i,n) v[i] = {a[i], b[i]}; 
+  priority_queue<ii> pq(all(v)); 
+
+  while (k--) {
+    auto [x, y] = pq.top(); 
+    if (x < 0) break; 
+    tot += x; pq.pop(); 
+    pq.push({x-y, y}); 
+  }
+  cout << tot << '\n'; 
 }
 
+
 int main(){
-  FIN;
-
-  int t; cin >> t;
-  while (t--) {
-    solve();
-    RAYA;
-  }
-
-
+  NaN;
+  int t = 1; 
+  cin >> t;
+  while (t--) solve();
   return 0;
 }
