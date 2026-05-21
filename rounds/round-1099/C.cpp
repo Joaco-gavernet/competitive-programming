5fad4daf17c0ebf63e0f4c59b5c0a1dfc200b25c
Joaquin Gavernet <joaquingavernet@gmail.com>
2026-05-21 18:27:37 -0300

    div2

---
+++ rounds/round-1099/C.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<bool> vb;
+typedef vector<int> vi;
+#define NaN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
+
+
+int f(int a) {
+  if (a == 1) return 0; 
+  if (a&1) return 1 + f(a + 1);
+  else return 1 + f(a / 2); 
+} 
+
+void solve() {
+  int n; cin >> n; 
+  vi a(n); forn(i,n) cin >> a[i]; 
+
+  unordered_map<int,int> cnt; 
+  priority_queue<ii> pq; 
+  forn(i,n) pq.push({f(a[i]), a[i]}), cnt[a[i]]++; 
+
+  ll steps = 0; 
+  while (SZ(pq)) {
+    auto [k, x] = pq.top(); pq.pop(); 
+    if (cnt[x] == n or k == 1) break; 
+
+    int z = --cnt[x]; 
+    if (z == 0) cnt.erase(x); 
+    if (x & 1) x++; 
+    else x /= 2; 
+    cnt[x]++; 
+    k--; 
+    steps++; 
+    pq.push({k, x}); 
+  } 
+
+  cout << steps + min(cnt[1], cnt[2]) << '\n';
+}
+
+
+int main(){
+  NaN;
+  int t = 1; 
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
---
+++ rounds/round-1099/C2.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define NaN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
+
+unordered_map<ll,ll> memo; 
+
+ll f(ll a) {
+  if (a == 1) return 0; 
+  if (memo[a] != 0) return memo[a]; 
+  if (a&1) return memo[a] = 1 + f(a + 1);
+  else return memo[a] = 1 + f(a / 2); 
+} 
+
+void solve() {
+  int n; cin >> n; 
+  vi a(n); forn(i,n) cin >> a[i]; 
+
+  unordered_map<ll,ll> cnt; 
+  priority_queue<ii> pq; 
+  forn(i,n) {
+    if (cnt[a[i]] == 0) pq.push({f(a[i]), a[i]}); 
+    cnt[a[i]]++; 
+  }
+
+  ll steps = 0; 
+  while (SZ(pq)) {
+    auto [k, x] = pq.top(); pq.pop(); 
+    if (cnt[x] == n or k == 1) break; 
+
+    steps += cnt[x]; 
+    ll aux = cnt[x]; 
+    cnt[x] = 0; 
+    if (x & 1) x++; 
+    else x /= 2; 
+    k--; 
+
+    if (cnt[x] == 0) pq.push({k, x}); 
+    cnt[x] += aux; 
+    pq.push({k, x}); 
+  } 
+
+  cout << steps + min(cnt[1], cnt[2]) << '\n';
+}
+
+
+int main(){
+  NaN;
+  int t = 1; 
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
