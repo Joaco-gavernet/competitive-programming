e4df9a13a795934c809a5a449026009e8f88f420
Joaquin Gavernet <joaquingavernet@gmail.com>
2026-05-28 20:23:34 -0300

    keep streak.

---
+++ random/1991C-ii.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define NaN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
+
+
+void reti() {
+  cout << "-1\n";
+  return; 
+} 
+
+void solve() {
+  int n; cin >> n;
+  vi a(n); forn(i,n) cin >> a[i]; 
+
+  forn(i,n) if ((a[i] & 1) != (a[0] & 1)) return reti(); 
+
+  vi ans; 
+  while (SZ(ans) < 40) {
+    sort(all(a)); 
+    if (a.back() <= 1) break; 
+    ll x = a.back() / 2; 
+    ans.pb(x); 
+    forn(i,n) a[i] -= x, a[i] = abs(a[i]); 
+  } 
+  if (a.back() == 1) ans.pb(1); 
+
+  cout << SZ(ans) << '\n';
+  for (auto x : ans) cout << x << ' ';
+  cout << '\n'; 
+}
+
+
+int main(){
+  NaN;
+  int t = 1; 
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
