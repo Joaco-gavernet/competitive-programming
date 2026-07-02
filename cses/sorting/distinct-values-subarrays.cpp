98ce7dbcc9c000bdfbfc79398cd4dcf91644f37d
Joaco Gavernet <joaquingavernet@gmail.com>
2026-07-02 01:39:11 -0300

    cses?

---
+++ cses/sorting/distinct-values-subarrays.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<bool> vb;
+typedef vector<ll> vi;
+#define NaN ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define forr(i, a, b) for(ll i = (a); i < (ll) (b); i++)
+#define forn(i, n) forr(i, 0, n)
+#define pb push_back
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define SZ(x) int((x).size()) 
+#define RAYA cerr << "===============================" << endl
+
+
+
+
+int main(){
+  NaN;
+
+  int n; cin >> n; 
+  vi x(n); forn(i,n) cin >> x[i]; 
+
+  ll tot = 0; 
+  map<int,int> h; 
+  int l = 0; 
+  forn(r,n) {
+    h[x[r]]++; 
+    while (h[x[r]] > 1) h[x[l++]]--;
+    tot += r - l + 1;
+  } 
+
+  cout << tot << '\n'; 
+
+  return 0;
+}
