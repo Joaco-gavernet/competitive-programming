9620e94135d7a028a7ea163343311aa37122e30d
Joaco Gavernet <joaquingavernet@gmail.com>
2026-07-31 12:33:16 -0300

    upsolved tree dp.

--- tc-laplata-2026/simulacion7/B2.cpp
+++ tc-laplata-2026/simulacion7/B2.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
+// neal Debugger
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+ 
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+
 using ll = long long;
 using vi = vector<ll>;
 using vb = vector<bool>;
@@ -17,6 +25,15 @@ using ii = pair<ll,ll>;
 #define RAYA cerr << "=============================\n"
 
 
+ll prefil(int x, vector<vi>& g, vi& toleaf) {
+  if (SZ(g[x]) == 0) return toleaf[x] = 0LL; 
+  for (auto y : g[x]) {
+    prefil(y, g, toleaf); 
+    toleaf[x] = min(toleaf[x], toleaf[y] + 1); 
+  }
+  return toleaf[x];
+} 
+
 int main() {
     FIN;
 
@@ -27,11 +44,33 @@ int main() {
         g[p].pb(i+1);
     }
 
-    vi path; 
-    function<void(int, int)> dfs = [&](int x, int prev = -1) {
-      for (auto y : g[x]) if (y != prev) dfs(y, x); 
+    vi toleaf(n, 1LL<<60); 
+    prefil(0, g, toleaf); 
+
+
+    vi path, jmp(n); 
+    function<void(ll)> f = [&](ll x) {
+      path.pb(x); 
+      int nxt = min((ll)SZ(path) - 1, max(0LL, SZ(path) - k - 1 + toleaf[x])); 
+      jmp[x] = path[nxt];
+      for (auto y : g[x]) f(y); 
+      path.pop_back(); 
     }; 
-    dfs(0); 
+    f(0); 
+
+    forn(i,n) forn(_,60) jmp[i] = jmp[jmp[i]]; 
+
+    vi dp(n); 
+    function<ll(ll)> calc = [&](ll x) {
+      ll best = 0;
+      if (SZ(g[x]) == 0) dp[jmp[x]]++; 
+      for (auto y : g[x]) {
+        best = max(best, calc(y)); 
+      } 
+      dp[x] += best;
+      return dp[x]; 
+    }; 
+    calc(0); 
 
     cout << dp[0] << "\n";
     return 0;
