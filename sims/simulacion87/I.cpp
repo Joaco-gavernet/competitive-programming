4467c9099897209e7e315d5fddd408e3191aa8b9
Joaco Gavernet <joaquingavernet@gmail.com>
2026-09-09 20:37:30 -0300

    update.

--- sims/simulacion87/I.cpp
+++ sims/simulacion87/I.cpp
@@ -47,6 +47,7 @@ const int S = 6;
 typedef struct item *pitem;
 struct item {
   int pr,key; pitem l,r;
+  ll sz;
   vi cnt; 
   // cnt[0] = "AB..." 
   // cnt[1] = "AA..." 
@@ -54,69 +55,91 @@ struct item {
   // cnt[3] = "BB..." 
   // cnt[4] = "ABAB..." 
   // cnt[5] = "BABA..." 
-  item(int key):pr(rand()),key(key),l(0),r(0) { cnt = {key, key, -key, -key, key, -key}; }
+  item(int key):pr(rand()),key(key),l(0),r(0),sz(0) { 
+    cnt = {key, key, -key, -key, key, -key}; 
+    dbg(key, cnt); 
+  }
 };
 
 vi cnt(pitem t){return t?t->cnt:vi(S);}
+ll cnti(pitem t, int i) { return t ? t->cnt[i] : 0ll; } 
+ll get_sz(pitem t) { return t ? t->sz : 0ll; } 
+pair<vi, ll> combine(pitem lef, pitem rig) {
+  vi ans = cnt(rig); 
+  vi cl = cnt(lef); 
+  ll len = get_sz(rig); 
+  ll sz = get_sz(lef) + get_sz(rig);
+  if (len&1) {
+    ll f = (len - 1) / 2; 
+    ans[0] += (f&1 ? cl[1] : cl[3]);  
+    ans[1] += (f&1 ? cl[2] : cl[0]);  
+    ans[2] += (f&1 ? cl[3] : cl[1]);  
+    ans[3] += (f&1 ? cl[0] : cl[2]);  
+    ans[4] += cl[5];  
+    ans[5] += cl[4];  
+  } else {
+    ll f = len / 2; 
+    ans[0] += (f&1 ? cl[2] : cl[0]);  
+    ans[1] += (f&1 ? cl[3] : cl[1]);  
+    ans[2] += (f&1 ? cl[0] : cl[2]);  
+    ans[3] += (f&1 ? cl[1] : cl[3]);  
+    ans[4] += cl[4];  
+    ans[5] += cl[5];  
+  } 
+  return {ans, sz}; 
+}
 void upd_cnt(pitem t) {
   if (t) {
-    vi cl = cnt(t->l); 
-    vi cr = cnt(t->r); 
-    t->cnt = vi(S); 
-
-    ll len = t->l->r - t->l->l + 1; 
-    if (len&1) {
-      ll f = (len - 1) / 2; 
-      t->cnt[0] = cr[(f&1 ? cr[1] : cr[3])];  
-      t->cnt[1] = cr[(f&1 ? cr[2] : cr[0])];  
-      t->cnt[2] = cr[(f&1 ? cr[3] : cr[1])];  
-      t->cnt[3] = cr[(f&1 ? cr[0] : cr[2])];  
-      t->cnt[4] = cr[5];  
-      t->cnt[5] = cr[4];  
-    } else {
-      ll f = len / 2; 
-      t->cnt[0] = cr[(f&1 ? cr[2] : cr[0])];  
-      t->cnt[1] = cr[(f&1 ? cr[3] : cr[1])];  
-      t->cnt[2] = cr[(f&1 ? cr[0] : cr[2])];  
-      t->cnt[3] = cr[(f&1 ? cr[1] : cr[3])];  
-      t->cnt[4] = cr[4];  
-      t->cnt[5] = cr[5];  
-    } 
+    auto [v, sz] = combine(t, t->r);
+    t->cnt = v;
+    t->sz = sz; 
+    auto [u, szu] = combine(t->l, t); 
+    t->cnt = u;
+    t->sz = szu; 
   } 
 }
 
-void split(pitem t, int key, pitem& l, pitem& r){ // l: <= key, r: > key
-  if(!t)l=r=0;
-  else if(key<t->key)split(t->l,key,l,t->l),r=t;
-  else split(t->r,key,t->r,r),l=t;
-  upd_cnt(t);
-}
-
-void insert(pitem& t, pitem it){
-  if(!t) t = it;
-  else if(it->pr > t->pr) split(t,it->key,it->l,it->r),t=it;
-  else insert(it->key < t->key ? t->l : t->r, it);
-  upd_cnt(t);
+void split(pitem t, int key, pitem& l, pitem& r){ // l: < key, r: >= key
+    if(!t)l=r=0;
+    else if(key<=t->key)split(t->l,key,l,t->l),r=t;
+    else split(t->r,key,t->r,r),l=t;
+    upd_cnt(t);
 }
 
 void merge(pitem& t, pitem l, pitem r){
-  if(!l||!r)t=l?l:r;
-  else if(l->pr>r->pr)merge(l->r,l->r,r),t=l;
-  else merge(r->l,l,r->l),t=r;
-  upd_cnt(t);
+    if(!l||!r)t=l?l:r;
+    else if(l->pr>r->pr)merge(l->r,l->r,r),t=l;
+    else merge(r->l,l,r->l),t=r;
+    upd_cnt(t);
+}
+
+void insert(pitem& t, pitem it){
+    if(!t)t=it;
+    else if(it->pr>t->pr)split(t,it->key,it->l,it->r),t=it;
+    else insert(it->key<t->key?t->l:t->r,it);
+    upd_cnt(t);
 }
 
 void erase(pitem& t, int key){
-  if(t->key==key)merge(t,t->l,t->r);
-  else erase(key<t->key?t->l:t->r,key);
-  upd_cnt(t);
+    if(t->key==key)merge(t,t->l,t->r);
+    else erase(key<t->key?t->l:t->r,key);
+    upd_cnt(t);
 }
 
 void DFS(pitem t) { //Good for debug
+  if (!t) return; 
+  cout << "(";
   if(t->l != NULL) DFS(t->l); 
-  cout << char('A' + t->key);
+  cout << ") ";
+  cout << t->key << ' ';
+  cout << "(";
   if(t->r != NULL) DFS(t->r);
+  cout << ")";
 }
+
+ostream& operator << (ostream& os, item D) {
+  return os << "(" << D.key << " -> cnt = [" << D.cnt[0] << ", " << D.cnt[1] << ", " << D.cnt[2] << ", " << D.cnt[3] << ")\n"; 
+} 
 
 int main() {
   FIN; 
@@ -128,35 +151,55 @@ int main() {
 
   vi d(n); forn(i,n) d[i] = b[i] - a[i]; 
   sort(all(d)); 
-  dbg(a);
-  dbg(b);
-  dbg(d); 
+  dbg(sum); dbg(a); dbg(b); dbg(d); 
 
-  pitem treap; 
-  forn(i,n) {
-    dbg(i); 
-    item D(d[i]); 
-    insert(treap, &D); 
-  } 
-  // DFS(treap); 
+  pitem root = NULL; 
+  forn(i,n) insert(root, new item(d[i])); 
+  
+  cerr << "DFS: "; DFS(root); cout << '\n'; 
+  pitem neg, pos; 
+  split(root, 0, neg, pos); 
+  cerr << "neg: "; DFS(neg); cout << '\n'; 
+  cerr << "pos: "; DFS(pos); cout << '\n'; 
+  ll tot = cnti(pos, 0); 
+  ll len = get_sz(pos); 
+  dbg(tot, len); 
+  dbg(cnt(neg)); 
+  dbg(cnt(pos)); 
+  if (len&1) tot += cnti(neg, 5);
+  else tot += cnti(neg, 4); 
+  cout << tot << '\n'; 
+  merge(root, neg, pos);
 
   forn(i,m) {
-    int t, x, y; cin >> t >> x >> y; 
-    t--; 
-    dbg(t, x, y); 
-    erase(treap, d[t]); 
+    RAYA; 
+    int t, x, y; cin >> t >> x >> y; t--; 
+    // dbg(t, x, y); 
+
+    ll prev = d[t]; 
+    sum -= d[t]; 
+    erase(root, d[t]); 
     d[t] = y - x; 
-    item D(d[t]); 
-    insert(treap, &D); 
+    ll nxt = d[t];
+    sum += d[t]; 
+    insert(root, new item(d[t])); 
+    dbg(prev, nxt); 
 
     // find answer split non-negative range and negative range (different strategies) 
-    pitem l, r; 
-    split(treap, -1, l, r); 
-    ll diff = l->cnt[0]; 
-    ll len = l->r - l->l + 1; 
-    if (len&1) diff += r->cnt[5]; 
-    else diff += r->cnt[4]; 
-    cout << (sum - diff) / 2 << '\n'; 
+    cerr << "DFS: "; DFS(root); cout << '\n'; 
+    pitem neg, pos; 
+    split(root, 0, neg, pos); 
+    cerr << "neg: "; DFS(neg); cout << '\n'; 
+    cerr << "pos: "; DFS(pos); cout << '\n'; 
+    ll tot = cnti(pos, 0); 
+    ll len = get_sz(pos); 
+    dbg(tot, len); 
+    dbg(cnt(neg)); 
+    dbg(cnt(pos)); 
+    if (len&1) tot += cnti(neg, 5);
+    else tot += cnti(neg, 4); 
+    cout << tot << '\n'; 
+    merge(root, neg, pos);
   } 
 
 
